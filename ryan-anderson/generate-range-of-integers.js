9c693d3cb7dd889e3cf60773282d483b8fd8bcac
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-03-24 19:34:30 -0500

    first two katas

---
+++ ryan-anderson/generate-range-of-integers.js
@@ -0,0 +1,11 @@
+function generateRange(min, max, step){
+    let s = min; //Creates a variable for adding the step value over each iteration
+    let a = [min]; //Initializes the output array with the first value
+    for (i = 1; i < (((max-min)/step)+1); i++) { //Through testing, I arrived at this formula to generate the correct amount of numbers in the array
+      s = s + step; //Adds the step to the previous array entry to create the next one
+      if (s <= max) { //Ensures that the array stops before exceeding the max value
+      a[i] = s; //Adds the next value to the array, starting at the second position because min is already in the first position
+        }
+        }
+      return a
+    }
---
+++ ryan-anderson/sum-of-numbers.js
@@ -0,0 +1,13 @@
+function getSum(a, b)
+{ let s = 0; //Creates sum variable to store sum over each iteration
+ if (a == b) { //if the two numbers are the same, return that number
+  s = a;
+  } else if (a < b) { 
+      for (let i = a; i <= b; i++)  //iterates on each number in the test array
+      s = s + i; //add each number to the stored sum of all previous numbers
+    } else if (a > b){
+      for (let i = b; i <= a; i++)
+       s = s + i;
+    }
+ return s //the test compares the first output to the exptected output, so the sum must be stored and not returned until the very end
+}
