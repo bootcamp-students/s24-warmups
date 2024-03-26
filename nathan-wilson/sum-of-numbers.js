0b9089d8e30a13ad0fdd34ed9332a0784b6947f5
nwils000 <nwils000@gmail.com>
2024-03-26 09:08:12 -0400

    Solved sum of numbers kata

---
+++ nathan-wilson/sum-of-numbers.js
@@ -0,0 +1,16 @@
+function getSum(a, b) {
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+  let sum = null;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+// Figure out which int is smaller a || b
+// Make the smaller int the start of a loop
+// Make the larger one the end of a loop
+// Add each number in the loop to a sum variable
+// return the sum
