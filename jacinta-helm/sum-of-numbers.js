161757725b5848ab349ab099a4270d7cebc8f512
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-03-26 12:49:03 -0400

    sum-of-numbers.js

--- jacinta-helm/sum-of-numbers.js
+++ jacinta-helm/sum-of-numbers.js
@@ -1,1 +1,17 @@
+function GetSum = (a, b) {
+  let min = Math.min(a, b),
+      max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
 
+/* S = n(a + l)/2 (sum of integers formula)
+
+S = sum of the consecutive integers
+n = number of integers
+a = first term
+l = last term
+
+Math.min is the lowest value
+Math.max is the highest value
+Add 1 to Math.min value and Math.max value to include into the sum
+*/
