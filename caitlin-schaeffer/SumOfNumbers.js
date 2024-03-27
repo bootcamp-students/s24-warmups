8ee7fe4870bd7815c84eecbd2e2e332a74e883bd
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-03-27 15:30:06 -0400

    edit

---
+++ caitlin-schaeffer/SumOfNumbers.js
@@ -0,0 +1,4 @@
+Sum of Numbers kata
+Given two integers a and b, which can be positive or negative, 
+find the sum of all the integers between and including them and return it. 
+If the two numbers are equal return a or b.
