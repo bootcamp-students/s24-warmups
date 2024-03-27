3086992eb78c4282077dfecfa4efbe3a2b6fe168
Owen Coombs <53359277+OwenCoombs@users.noreply.github.com>
2024-03-26 20:44:43 -0400

    Create generate-range-of-integers.js

---
+++ owen-coombs/generate-range-of-integers.js
@@ -0,0 +1,17 @@
+function generateRange(min, max, step) {
+  let sum = [];
+  if (max - min < step) {
+    sum.push(min);
+  } else {
+    for (let i = min; i <= max; i += step) {
+      sum.push(i);
+    }
+  }
+  return sum;
+}
+// Create an empty array to store the generated numbers
+// Check if the difference between max and min is less than the step
+// If yes, add only the min value to the array
+// If the difference is equal to or greater than the step, start a loop from min to max with step increments
+// Add each value within the range to the array
+// Return the generated array
