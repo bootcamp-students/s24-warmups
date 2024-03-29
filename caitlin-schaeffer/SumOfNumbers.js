e8bc2349d98130aec6dc67f092e2449d786a6688
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-03-29 09:59:11 -0400

    updated java

--- caitlin-schaeffer/SumOfNumbers.js
+++ caitlin-schaeffer/SumOfNumbers.js
@@ -1,4 +1,28 @@
-Sum of Numbers kata
-Given two integers a and b, which can be positive or negative, 
-find the sum of all the integers between and including them and return it. 
-If the two numbers are equal return a or b.
+// Sum of Numbers kata
+// Math.min() and Math.max() can be used to find the lowest or highest value in a list of arguments.
+// Simplest solution is the min/max function, or you could do if-else if-else functions establishing the values of a and b to return the correct sums
+// function getSum(a,b); {
+// let min = Math.min(a, b);
+// let  max = Math.max(a, b);
+   // return (max - min + 1) * (min + max) / 2;
+// }
+// return sum 
+// This code returned error, so try the if/else stuff
+function getSum(a,b); {
+let sum = 0
+// set the sum to zero, which is normal, but then set the function to produce a first
+if (a === b) {
+    sum = a;
+}
+else if (a < b) {
+    // set the integer to a
+    for (let i = a; i >= b; i--) {
+        sum += i }
+    }
+    else {
+        for (let i = b; i >= a; i--) {
+            sum += i;
+        }
+    }
+    return Sum
+}
