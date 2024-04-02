6c515c53923ab6bf4a965f7f905816d4eea5bbb1
nwils000 <nwils000@gmail.com>
2024-04-02 08:32:37 -0400

    Finished isDivisible

---
+++ nathan-wilson/is-n-divisible-by-x-and-y.js
@@ -0,0 +1,10 @@
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0;
+}
+
+// n is the number to test to see if it is divisible by x and y. All are integers
+// reuturn value is a boolean
+// n3 x3 y4 is false bevause 3 is not divisible by 4
+// I may return the wrong value. I may have a logic issue.
+// Take 9 and test if it divided by x oe y has a remainder. If it does return false. If not return true.
+// I can do this! Its okay if i dont get it first try
