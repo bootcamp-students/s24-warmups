c3943399c2a8cefa601db848170c18de05c49129
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-02 11:02:42 -0400

    working on division

--- caitlin-schaeffer/IsNdivisibleByXandY.js
+++ caitlin-schaeffer/IsNdivisibleByXandY.js
@@ -1,1 +1,14 @@
-Is n divisible by x and y?
+/*
+Params: Create a function that checks if a number n is divisible by two numbers x AND y. 
+All inputs are positive, non-zero numbers.
+Return value: should return true or false
+Exmaple: n = 10; x = 5; y = 2; true (10 is divisible by both x and y)
+Ideas: I think a good code could be something that checks if the product 
+of n/x and n/y is a decimal. If it's a decimal return false, if it isn't it's true
+*/
+
+function isDivisible(n, x, y) {
+    if (true) {(n/x), (n/y) = whole number;}
+    
+    else (false) {(n/x) = decimal; (n/y) = decimal;}
+  }
