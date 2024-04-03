d39d63f47dff9c628c45787c7d7409ba9c17f4ce
Duke McClellan <162177816+dmcclellan3@users.noreply.github.com>
2024-04-03 08:31:25 -0400

    Create can-you-make-it.js

---
+++ duke-mcclellan/can-you-make-it.js
@@ -0,0 +1,10 @@
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   let distanceToPump = 50;
+//   let mpg = 25;
+//   let fuelLeft = 2;
+//   Recieving number and will return boolean
+//  miles per gallon multiplied by how much fuel is left
+// if how far is >= how far you need to go you will make it 
+  
+  return (distanceToPump <= mpg * fuelLeft ? true : false) 
+};
