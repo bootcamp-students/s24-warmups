a2c6caaa2bd11f96df64ad0dcb2f76c9d777aa66
hpknollenberg <hpknollenberg@gmail.com>
2024-04-04 08:16:18 -0400

    pseudocode for day 2

--- henry-knollenberg/day-02/is-n-divisible-by-x-and-y.js
+++ henry-knollenberg/day-02/is-n-divisible-by-x-and-y.js
@@ -1,3 +1,8 @@
+/* 
+if n divided by x is an integer and n divided by y is an integer then return true
+else return false
+*/
+
 function isDivisible(n, x, y) {
     if (Number.isInteger(n / x) && Number.isInteger(n / y)) {
       return true;
