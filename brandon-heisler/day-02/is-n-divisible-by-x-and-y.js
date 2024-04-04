eaa7ad3358bb257db77ecbcf9b3bc6729281855a
Skell87 <136534563+Skell87@users.noreply.github.com>
2024-04-04 09:05:07 -0400

    Update is-n-divisible-by-x-and-y.js

--- brandon-heisler/day-02/is-n-divisible-by-x-and-y.js
+++ brandon-heisler/day-02/is-n-divisible-by-x-and-y.js
@@ -6,3 +6,9 @@ function isDivisible(n, x, y) {
     return false;
   }
 }
+/*
+im given a number and need to see if its divisible by 2 other numbers, x and y.
+i can write an if statement to compare compare the remainders of each, if
+both = 0 then return true, else return false.
+
+*/
