76776a073b8a8ee7ca48795e1463e7a31c890955
Skell87 <136534563+Skell87@users.noreply.github.com>
2024-04-04 08:57:10 -0400

    Create remove-string-spaces.js

---
+++ brandon-heisler/day-04/remove-string-spaces.js
@@ -0,0 +1,21 @@
+function noSpace(x){
+  return x.replaceAll(" ","")
+}
+
+/*
+
+p: i have x which will give me a string. I will have characters
+and spaces in that string. i need to manipulate it to remove the blanks.
+
+R: should return a string with no spaces
+
+E: "8 J B" --> "8JB"
+
+C: im concerned that i dont know what will delete a space.
+it should be a simple thing. read the string, return it differently.
+
+p: all i have to do is learn, take it step by step, i am capable.
+
+t: i discovered .replaceAll, learned that i can substitute " " (a space) with "" (no character)
+
+*/
