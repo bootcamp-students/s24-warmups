49eb722e148fff38450a7ef43e97d63190746830
Duke McClellan <162177816+dmcclellan3@users.noreply.github.com>
2024-04-04 08:58:14 -0400

    remove-string-spaces-day-04.js

---
+++ duke-mcclellan/remove-string-spaces-day-04.js
@@ -0,0 +1,6 @@
+//Return value needs to remove whitespace from the string - create a new string
+//string trim? reduce function? join method?
+
+function noSpace(x){
+ return x.replaceAll(' ','');
+}
