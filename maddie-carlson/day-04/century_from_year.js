048337770649b10d3d3eb79a53330465588c00e2
Madeline Carlson <162224573+SpookyLamb@users.noreply.github.com>
2024-04-04 08:01:53 -0400

    Create century_from_year.js

---
+++ maddie-carlson/day-04/century_from_year.js
@@ -0,0 +1,9 @@
+function century(year) {
+  // calculate century from a given year
+  // parameter is just year, which should be a number
+  // should return the century, also a number
+  // this number can be calculated from Math.ceil(year / 100)
+  
+  let century = Math.ceil(year / 100);
+  return century;
+}
