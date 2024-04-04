b5d6cf018cbd71967fb7bc524125865145d023f0
Madeline Carlson <162224573+SpookyLamb@users.noreply.github.com>
2024-04-04 08:09:17 -0400

    Create no_spaces.js

---
+++ maddie-carlson/day-04/no_spaces.js
@@ -0,0 +1,6 @@
+function noSpace(x) {
+  // parameter is x, which is a string
+  // should return another string with any and all spaces removed
+  // this can be accomplished by replacing all spaces with an empty string with replaceAll()
+  return x.replaceAll(" ", "");
+}
