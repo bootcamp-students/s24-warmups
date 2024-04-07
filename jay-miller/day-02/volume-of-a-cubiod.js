77016edc5f1570bd49c72bfd661d2f7e54bd3e18
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-04-07 00:50:49 -0400

    Jay week 1 feedback

---
+++ jay-miller/day-02/volume-of-a-cubiod.js
@@ -0,0 +1,10 @@
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
+
+/*
+  Ember's Feedback:
+  good job
+*/
--- jay-miller/day-03/day-3-will-you-make-it.js
+++ jay-miller/day-03/day-3-will-you-make-it.js
@@ -2,3 +2,9 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   // TODO-Multiply mpg by fuelLeft to get distanceToPump
  return mpg * fuelLeft >= distanceToPump ? true : false;
 };
+
+/*
+  Ember's Feedback:
+  Good job
+  The ternary is unnecessary, >= as an operator will return a boolean value already
+*/
