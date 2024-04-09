72ebbbe8ff3f599e940e49cedc604a9c11012f55
Madeline Carlson <162224573+SpookyLamb@users.noreply.github.com>
2024-04-09 08:31:02 -0400

    Update coin_combos.js

--- maddie-carlson/day-06/coin_combos.js
+++ maddie-carlson/day-06/coin_combos.js
@@ -1,4 +1,7 @@
 function coinCombo (cents) {
+  //needs to return an array with the least amount of coins, going from pennies to nickels to dimes to quarters
+  //to accomplish this, we need to count in reverse, largest coins first, then smallest coins
+  //this can be done by using while loops and gradually subtracting from our cents until the current coin is too large
   let coins = [0, 0, 0, 0];
   
   //quarters
