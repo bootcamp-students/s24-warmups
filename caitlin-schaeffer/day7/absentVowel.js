79c7557c035e02a88e9a4551ab3113f7ca8cafdc
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-09 13:11:02 -0400

    finished absent vowel

--- caitlin-schaeffer/day7/absentVowel.js
+++ caitlin-schaeffer/day7/absentVowel.js
@@ -8,7 +8,7 @@ U has an index of 4.
 Notes: There is no need for string validation and every sentence given will contain all vowels but one. 
 Also, you won't need to worry about capitals.
 Return: a vowel
-Solutions: 
+Solutions: run a for loop to check if vowel is in the string
 
 Given Code:
 function absentVowel(x){
@@ -18,6 +18,53 @@ function absentVowel(x){
 
 // I want to write a code that runs through every single letter of a given sentence and returns if something is missing
 // If this doesn't work, just make an if else running through every single vowel 
+// some comments left by Ember in explanation
+// you can rename stuff in their given code!
+
+function absentVowel(inputString) {
+  // array of vowels I will search for
+  // I want to return a number associated with each vowel,
+  // the number is associated with the vowels index in this array
+  const vowels = ['a','e','i', 'o', 'u'];
+  
+  // I will iterate over my vowels array to search my input string for each vowel
+  // took out the 'i' & 'j'
+  for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex++) {
+
+    let found = false;
+    let currentVowel = vowels[vowelsIndex];
+
+    // I will iterate over the input string to search for the current vowel
+    for (let inputStringIndex = 0; inputStringIndex < inputString.length; inputStringIndex++) {
+      
+      let currentCharacter = inputString[inputStringIndex]
+      
+      if (currentVowel === currentCharacter) {
+        found = true;
+        // break out of this for loop if I found the vowel
+        break;
+      }
+    }
+
+    // If I did not find the current vowel in the input string,
+    // I will return the index number of the missing vowel
+    if (!found) {
+      return vowelsIndex;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+Broken code/pre-used code: 
 
 function absentVowel(x){
     let absentVowel = [];
@@ -43,19 +90,6 @@ function absentVowel(x){
       }
       return x;
       }
-
-
-
-
-
-
-
-
-
-
-/*
-Broken code: 
-
 
 function absentVowel(x){
     const vowels = [];
