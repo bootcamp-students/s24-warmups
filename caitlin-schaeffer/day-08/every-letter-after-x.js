807626f505c037cce8a5691338c3f02b85232632
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-10 09:04:40 -0400

    started on capitals

--- caitlin-schaeffer/day-08/every-letter-after-x.js
+++ caitlin-schaeffer/day-08/every-letter-after-x.js
@@ -1,8 +1,44 @@
 /*
-Params: 
-Return: 
+Params: Imagine a book author has hired us to create a function that takes his book as a string as input 
+and finds all instances of a letter that we will call 'x' (case-sensitive) and turns all the 
+letters after 'x' into a capital letter.
+For example:
+makeEveryLetterAfterXCaps('and larry walked into the park late to chase a squirrel', 'l')
+Should return:
+"and lArry walKed into the park lAte to chase a squirrel"
+If the letter after x is already in caps it should stay in caps. Note that the swapping occurs 
+considering the modified string, so makeEveryLetterAfterXCaps('aaaa', 'a') will result into "aAaA"
+Return: string
 Solution: 
 
 Given Code:
+var makeEveryLetterAfterXCaps = function (str, letter) {
+// enter solution here
 
+
+
+}
 */
+
+/*
+Initial ideas: Need to ascertain how JavaScript reads capital letters, then just make a function that 
+idenitifes the capital, then makes it lowercase, then capitalizes the next letter and concates everything?
+*/
+
+
+var makeEveryLetterAfterXCaps = function (str, letter) {
+    let str = [""];
+    let letter = [""];
+ // introduce something to search for capital letters
+    const letterIsUpper = (str) => {
+        for (let i= 0; i < str.length; i++) {
+            // this identifies that the string does not have a capital letter
+            if (str[i] !== str[i].toUpperCase()) {
+                return false; //will return false (no capitals)
+            } else if (str[i] === str[i].toUpperCase()) {
+                // now write a function to change the letter after the identified letter to capital
+            }
+        }
+    }   
+    
+    }
--- caitlin-schaeffer/day-08/who-likes-it.js
+++ caitlin-schaeffer/day-08/who-likes-it.js
@@ -1,8 +1,24 @@
 /*
-Params: 
-Return: 
+Params: You probably know the "like" system from Facebook and other pages. 
+People can "like" blog posts, pictures or other items. We want to create the text that should be displayed 
+next to such an item. Implement the function which takes an array containing the names of people that like 
+an item. It must return the display text as shown in the examples: 
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+Return: a string
 Solution: 
 
 Given Code:
-
+function likes(names) {
+  // TODO
+}
 */
+
+/* First thought:
+A function that adds an array to the string 'likes this' but must include some parameter that only lets it
+display two names and then displays an additional number of people ' and others like this'
+*/
+
+function likes(names) {
+    // TODO
+  }
