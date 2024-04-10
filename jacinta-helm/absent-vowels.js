6cbdf77a900c2bdb2cf853784844c9bcc7889f03
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-04-10 06:50:38 -0400

    Update absent-vowels.js

--- jacinta-helm/absent-vowels.js
+++ jacinta-helm/absent-vowels.js
@@ -1,1 +1,20 @@
+function absentVowel(x){
+ let vowels = ["a","e","i","o","u"];
+  for (const vowel of vowels) {
+//    console.log(vowel);
+    if (!x.includes(vowel)) {
+      return vowels.indexOf(vowel);
+        }
+    }
+    return ;
+  }
+//   } console.log(vowels.indexOf('a', );
+//     (vowels.indexOf('o',3));
+
+    
+
+//p figure out which vowel is missing 
+//using for of() and indexOf()
+// a =0, e=1, i=2, o=3, u= 4 create an array. 
+// 
 
