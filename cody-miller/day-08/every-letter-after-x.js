b47bc265902351cc3602a8c467eddf6c3459ca64
Cody <c.jmiller17@yahoo.com>
2024-04-10 13:43:05 -0400

    Capital Latters Solved

--- cody-miller/day-08/every-letter-after-x.js
+++ cody-miller/day-08/every-letter-after-x.js
@@ -1,15 +1,34 @@
 const makeEveryLetterAfterXCaps = function (str, letter) {
-// test word :parameter  paRaMeter or paramTeR
-    let result = ""; //Strings are immutable, so a new string is necessary?
-    for (let i = 0; i < str.length; i++) {
-        result += str[i];
-        if (str.charAt(i) === letter && i < str.length - 1) {
-            result += str.charAt(i + 1).toUpperCase();
-            i++;
-        }
+  let result = "";
+  let capitalizeNext = false;
+
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i];
+
+    if (capitalizeNext) {
+      currentChar = currentChar.toUpperCase();
     }
-    return result;
-}
+
+    capitalizeNext = currentChar === letter;
+    result += currentChar;
+  }
+
+  return result;
+};
+
+/* Previous Code that almost solved the answer */
+// const makeEveryLetterAfterXCaps = function (str, letter) {
+// // test word :parameter  paRaMeter or paramTeR
+//     let result = ""; //Strings are immutable, so a new string is necessary?
+//     for (let i = 0; i < str.length; i++) {
+//         result += str[i];
+//         if (str.charAt(i) === letter && i < str.length - 1) {
+//             result += str.charAt(i + 1).toUpperCase();
+//             i++;
+//         }
+//     }
+//     return result;
+// }
 /*
 I have to use toUpperCase
 I have to identify a letter
