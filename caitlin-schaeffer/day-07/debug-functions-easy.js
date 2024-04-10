46a42ee4e5999fbccbd4a678540bbf3686e012e3
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-10 08:37:18 -0400

    completed debug kata

--- caitlin-schaeffer/day-07/debug-functions-easy.js
+++ caitlin-schaeffer/day-07/debug-functions-easy.js
@@ -2,7 +2,7 @@
 Params: Debug the functions: Should be easy, begin by looking at the code. Debug the code and the functions should work.
 There are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR)
 Return: a number (whatever each equation results)
-Solutions: 
+Solutions: right a for loop to perform each function, you're not just adjusting the given code
 
 Given Code:
 function multi(arr) {
@@ -17,15 +17,43 @@ function reverse(str) {
 */
 
 function multi(arr) {
-  return arr * arr;
+  let sum=1;
+  for (let i = 0; i < arr.length; i ++) {
+    sum = sum * arr[i];
+    console.log(sum)
+  }
+  return sum;
 
 }
 function add(arr) {
-  return arr + arr;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 
 }
 function reverse(str) {
-  return str.reverse();
+  let newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+    return newString
+}
+
+
+/*
+Broken Code:
+
+function reverse(str) {
+  let reversedString = 0;
+  while (str !=0) {
+    reversedString*=10;
+    str-=str%10;
+    str/=10;
+  }
+  return reversedString;
 
 }
 
+*/
