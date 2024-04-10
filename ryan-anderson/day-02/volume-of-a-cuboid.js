115394a22871e97ecc44e6b42bc2fd08958819c8
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-04-10 11:32:57 -0400

    Ryan week 1 feedback

---
+++ ryan-anderson/day-02/volume-of-a-cuboid.js
@@ -0,0 +1,12 @@
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    let volume = (length * width * height); // formula for volume
+    return volume
+  }
+}
+
+/*
+  Ember's Feedback:
+  Great job!
+  This kata was a little weird because it exposes you to Class syntax :D
+*/
--- ryan-anderson/remove-string-spaces.js
+++
@@ -1,6 +0,0 @@
-function noSpace(x){
-    let newx = x.split(" ").join("");
-     return newx
-    //find command to remove all " " characters from string
-    //replace spaces with ""
-   }
