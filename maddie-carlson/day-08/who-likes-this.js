4fd9e24aed322dfcd76113fb657ee14f532b3b6c
Madeline Carlson <162224573+SpookyLamb@users.noreply.github.com>
2024-04-10 08:42:18 -0400

    Create who-likes-this.js

---
+++ maddie-carlson/day-08/who-likes-this.js
@@ -0,0 +1,28 @@
+function likes(names) {
+  // takes an array of names and returns a string showing who likes what, like how Facebook does it
+  // returns a string detailing the likes
+  // can fail if names is the wrong type or the types inside are not strings
+    
+  //check for empty array
+  if (names.length == 0) {
+    return "no one likes this"
+  }
+  
+  let likes = ""
+  
+  //add names based on array length
+  //takes at most 3 names, or two names if it's four or more, instead having a number
+  
+  if (names.length == 1) {
+    likes = names[0] + " likes this"
+  } else if (names.length == 2) {
+    likes = names[0] + " and " + names[1] + " like this"
+  } else if (names.length == 3) {
+    likes = names[0] + ", " + names[1] + " and " + names[2] + " like this" //no oxford comma :(
+  } else { //more than 3
+    likes = names[0] + ", " + names[1] + " and " + String(names.length - 2) + " others like this" //just add to the count
+  }
+  
+  return likes
+  
+}
