90536d52529da393c240908c4311544335fd97d6
hpknollenberg <hpknollenberg@gmail.com>
2024-04-11 11:17:09 -0400

    make-every-letter-after-x-into-caps

--- henry-knollenberg/day-08/make-every-letter-after-x-into-caps.js
+++ henry-knollenberg/day-08/make-every-letter-after-x-into-caps.js
@@ -4,48 +4,36 @@
   
   Parameters: string, letter
   Return: copy of the string with the capitalized letters.
+  
+  
+  If nextCap is true, then capitalize current letter.
+  Add current letter.
+  Need to test if current letter is the letter. If so, set nextCap to true.
+  
   */
-
-  var makeEveryLetterAfterXCaps = function (str, letter) {
-    let capStr = "";  //Initializes the string to be returned
-    for (let i = 0; i < str.length; i++) { //For loop to loop through each character of the string 
-      capStr += str[i];  //Add each character to the new string
-      if (capStr[i] === letter) { //If the current letter equals the inputed letter,
-        i++;    //then skip to the next letter
-        if (i >= str.length) {
-          break;
-        }
-        capStr += str[i].toUpperCase(); //and capitalize the letter and add it to the new string 
-      }
-    }
-    return capStr;
-  }
-
-
-/*Ember -- I tried to restart, approaching it a bit differently, but I came 
-across the same issues. Both of these "solutions" pass the basic
-tests, but fail the random tests. Looking over the random tests, I 
-am almost sure that the test is faulty, but who knows (after all, your 
-solution that you sent me does pass them) - I'm probably
-missing something. Let me know if you can spot my issue. Thanks. My
-second attempt is below.*/
 
   var makeEveryLetterAfterXCaps = function (str, letter) {
     let strArray = str.split("");  
     let nextCap = false;
     let capStr = "";
     
-    for (const char of strArray) {
-      if (nextCap === true) {
-        capStr += char.toUpperCase();
-        nextCap = false;
-        continue;
-      } else {
-        capStr += char;
-      }
+    function checkSame (char, letter) {
       if (char === letter) {
         nextCap = true;
-      }       
+      } else {
+        nextCap = false;
+      }
+    } 
+    
+    for (const char of strArray) {
+      if (nextCap === true){
+        let curLetter = char.toUpperCase();
+        capStr += curLetter;
+        checkSame(curLetter, letter);
+      } else {
+        checkSame(char, letter);
+        capStr += char;
+      }
     } 
     return capStr;
   }
