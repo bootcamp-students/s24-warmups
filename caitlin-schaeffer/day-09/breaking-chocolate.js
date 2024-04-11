67468731539cc7f14e60a114ca5a5ee7c5e8bfbb
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-11 09:09:44 -0400

    done with multi d

---
+++ caitlin-schaeffer/day-09/breaking-chocolate.js
@@ -0,0 +1,23 @@
+/*
+Task: Your task is to split the chocolate bar of given dimension n x m into small squares.
+Params: n & m (both integers/irreal measurements of chocolate)
+Return: the number of breaks to make it 1
+Example:  (breakChocolate(5, 5) , 24)
+Concerns: 1,1 returns 0 because you can't break it down 
+Ideas: I think we could just divide by 1? or more so we need to minus 1 and return how many times that happens
+Explain:
+
+Given Function: 
+function breakChocolate(n,m) {
+  return 0;
+}
+*/
+
+// you're counting (returning) the *number of breaks* 
+
+function breakChocolate(n,m) {
+// first see how many times n can be broken into 1
+// second see how many times m can be broken into 1
+// third add the number of breaks from n and the number of breaks from m together
+    return 0;
+  }
