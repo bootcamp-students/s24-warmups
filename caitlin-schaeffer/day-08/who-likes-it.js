c7d82ddb88c2ac19c843d6802a5ad3b39c2a258a
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-12 08:43:35 -0400

    names

--- caitlin-schaeffer/day-08/who-likes-it.js
+++ caitlin-schaeffer/day-08/who-likes-it.js
@@ -1,10 +1,11 @@
 /*
-Params: You probably know the "like" system from Facebook and other pages. 
+Instructions: You probably know the "like" system from Facebook and other pages. 
 People can "like" blog posts, pictures or other items. We want to create the text that should be displayed 
 next to such an item. Implement the function which takes an array containing the names of people that like 
 an item. It must return the display text as shown in the examples: 
 []                                -->  "no one likes this"
 ["Peter"]                         -->  "Peter likes this"
+Params: Gives us an array called 'names'
 Return: a string
 Solution: 
 
@@ -15,10 +16,15 @@ function likes(names) {
 */
 
 /* First thought:
-A function that adds an array to the string 'likes this' but must include some parameter that only lets it
-display two names and then displays an additional number of people ' and others like this'
+A function that returns the string value of the array 'names' and then concates it to
+the phrase 'likes this' (with a few variables)
+If the function is empty it needs to return ('no one likes this')
+If the function returns more than three name it needs to list the first to name and 
+then count all of the other names to return ('name, name, name and 2 others like this')
 */
 
 function likes(names) {
-    // TODO
+    let names = ['']
+    
+
   }
