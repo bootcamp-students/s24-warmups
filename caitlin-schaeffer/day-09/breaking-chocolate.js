e61c09beefec434dbd9e3b4bb2053e03c0f6fed1
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-12 08:33:54 -0400

    finished chocolate

--- caitlin-schaeffer/day-09/breaking-chocolate.js
+++ caitlin-schaeffer/day-09/breaking-chocolate.js
@@ -5,7 +5,7 @@ Return: the number of breaks to make it 1
 Example:  (breakChocolate(5, 5) , 24)
 Concerns: 1,1 returns 0 because you can't break it down 
 Ideas: I think we could just divide by 1? or more so we need to minus 1 and return how many times that happens
-Explain:
+Explain: Basically you can calculate area and subtract 1 but you need to establish an else statement for if they provide two '0's'
 
 Given Function: 
 function breakChocolate(n,m) {
@@ -16,11 +16,14 @@ function breakChocolate(n,m) {
 // you're counting (returning) the *number of breaks* 
 
 function breakChocolate(n,m) {
-    let answer = 0  
-  if (m > 0 && n >0) {
-    answer = ((n * m) -1)
-    } else (m <= 0 && n <= 0); {
-      answer = 0
-    }
-     return answer;
-   }
+  let answer = 0  
+if (m > 0 && n > 0) {
+  answer = ((n * m) -1)
+  console.log(answer)
+  return answer 
+  } else (m <= 0 && n <= 0); {
+    answer = 0
+    console.log(answer)
+    return answer
+  }
+ }
