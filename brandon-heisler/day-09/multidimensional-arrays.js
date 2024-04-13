3c116bb8f712b6ec60470c6dd5dd329c22828a67
Skell87 <136534563+Skell87@users.noreply.github.com>
2024-04-12 21:59:45 -0400

    Create multidimensional-arrays.js

---
+++ brandon-heisler/day-09/multidimensional-arrays.js
@@ -0,0 +1,14 @@
+function getElement(array, indexes) {
+    let newArray = array
+    for(const index of indexes){
+      newArray = newArray[index];
+      
+    }
+  return newArray;
+}
+
+/*
+i need to return the array reference that matches what theyre giving me
+ they give me a multidimensional array and the index, 
+ i need to return the value in the array specified
+ by indexes.
