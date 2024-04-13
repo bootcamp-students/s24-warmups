566ba1663deaef4072e5d1dbcc0ac371c6587b5e
Skell87 <136534563+Skell87@users.noreply.github.com>
2024-04-12 22:01:00 -0400

    Create breaking-chocolate.js

---
+++ brandon-heisler/day-09/breaking-chocolate.js
@@ -0,0 +1,17 @@
+function breakChocolate(n,m) {
+ let result = n*m-1;
+  if (result > 0){
+    return result;
+  }
+  else{
+    return 0;
+  }
+}
+  
+/*
+my formula is n*m-1 to equal the number of breaks, if my result is greater than zero i need to print
+
+if my result isnt greater than zero then there is no chocolate. 
+
+have to do the math first and compare results, otherwise youll get -1
+*/
