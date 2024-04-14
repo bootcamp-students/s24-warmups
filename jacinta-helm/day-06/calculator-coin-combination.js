7d1fef7f97fa76c0ace89b458573ee053b5dbd01
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-04-14 18:48:43 -0400

    Jacinta week 2 feedback

--- jacinta-helm/day-06/calculator-coin-combination.js
+++ jacinta-helm/day-06/calculator-coin-combination.js
@@ -1,23 +1,23 @@
 
-function coinCombo (cents) {
+function coinCombo(cents) {
   let currentTotal = cents;
-  
+
   let coinArray = [0, 0, 0, 0];
   if (currentTotal >= 25) {
     coinArray[3] = Math.floor(currentTotal / 25);
     currentTotal = currentTotal - 25 * Math.floor(currentTotal / 25);
   }
-  
+
   if (currentTotal >= 10) {
     coinArray[2] = Math.floor(currentTotal / 10);
     currentTotal = currentTotal - 10 * Math.floor(currentTotal / 10);
   }
-  
+
   if (currentTotal >= 5) {
     coinArray[1] = Math.floor(currentTotal / 5);
     currentTotal = currentTotal - 5 * Math.floor(currentTotal / 5);
   }
-  
+
   if (currentTotal >= 1) {
     coinArray[0] = currentTotal;
   }
@@ -25,7 +25,23 @@ function coinCombo (cents) {
   return coinArray;
 }
 
-//p find a function that will return the coin combination of coins used in that amount
-// R The total amount should divide the amount of each coin starting with the largest 
-//amount to see how many more coins of different value can go into the coin amount.
-//E (1, [0,0,0,1]), (15, [0,1,1,0])
+// p write a function that will return the coin combination of coins used in that amount
+// R The total amount should divide the amount of each coin starting with the largest
+// amount to see how many more coins of different value can go into the coin amount.
+// E (1, [1,0,0,0]), (15, [0,1,1,0])
+
+/*
+  Ember's Feedback:
+  - Great solution!
+  - Really great job :D
+*/
+// Alternative solution
+function coinCombo(cents) {
+  let coins = [25, 10, 5, 1]
+  let remainder = cents
+  return coins.map(coin => {
+    let coinsInCents = Math.floor(remainder / coin)
+    remainder %= coin
+    return coinsInCents
+  }).reverse();
+}
---
+++ jacinta-helm/day-08/notes.js
@@ -0,0 +1,30 @@
+// Every letter after X caps
+
+//   let ans = "";
+//   for (let i = 1; i < str.length; i++) {
+//    if (str[i-1] === letter) {
+//     let temp = str[i].toUpperCase();
+//      ans += str[i-1];
+//      ans += temp
+//      console.log('Cap', temp, ans);
+//      i++
+// //      str[i + 1] = str[i + 1];
+//    } else {
+//      ans += str[i-1];
+//      console.log("no match", i, ans)
+//    } if (i = str.length-1) {
+//      ans+= str[i];
+//    }
+//     console.log('final', i, ans);
+//   }
+//       return ans;
+//   }
+
+
+
+
+//   let string = str.toUpperCase('x');{
+//     console.log(string.toUpperCase('x'));
+//   } return ;
+
+
