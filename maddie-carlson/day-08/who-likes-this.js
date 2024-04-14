f50a369842f2307a171136ff9d9dbe89fec8e95d
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-04-13 20:29:49 -0400

    Maddie week 2 feedback

--- maddie-carlson/day-08/who-likes-this.js
+++ maddie-carlson/day-08/who-likes-this.js
@@ -2,27 +2,46 @@ function likes(names) {
   // takes an array of names and returns a string showing who likes what, like how Facebook does it
   // returns a string detailing the likes
   // can fail if names is the wrong type or the types inside are not strings
-    
+
   //check for empty array
-  if (names.length == 0) {
+  if (names.length === 0) {
     return "no one likes this"
   }
-  
+
   let likes = ""
-  
+
   //add names based on array length
   //takes at most 3 names, or two names if it's four or more, instead having a number
-  
-  if (names.length == 1) {
+
+  if (names.length === 1) {
     likes = names[0] + " likes this"
-  } else if (names.length == 2) {
+  } else if (names.length === 2) {
     likes = names[0] + " and " + names[1] + " like this"
-  } else if (names.length == 3) {
+  } else if (names.length === 3) {
     likes = names[0] + ", " + names[1] + " and " + names[2] + " like this" //no oxford comma :(
   } else { //more than 3
     likes = names[0] + ", " + names[1] + " and " + String(names.length - 2) + " others like this" //just add to the count
   }
-  
+
   return likes
-  
+}
+/*
+  Ember's Feedback:
+  - Great job!
+  - Use === it will save you headaches down the road
+*/
+// Alternative solution
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return 'no one likes this';
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
 }
--- maddie-carlson/day-09/break-chocolate.js
+++ maddie-carlson/day-09/break-chocolate.js
@@ -1,16 +1,56 @@
-function breakChocolate(n,m) {
-  //takes two numbers describing the width and length of a chocolate bar 
+function breakChocolate(n, m) {
+  //takes two numbers describing the width and length of a chocolate bar
   //returns how many times the chocolate to reduce it into 1x1 chunks
   //needs to return zero if anything weird is sent in
-  
+
   if (!n || !m) { //falsey vars return 0
     return 0;
   }
-  
+
   if (n == 1 && m == 1) { //single unit chocolate already
     return 0;
   }
-  
+
   //should be able to solve the problem by multiplying n * m - 1 to get the breaks
   return (n * m) - 1;
+}
+
+/*
+  Ember's Feedback:
+  - Good job!
+*/
+// Params - n: number, m: number
+// Return - number
+// Example - (2, 2) => 3; (0, 4) => 0
+// Concern - If n or m are NaN, will my code return NaN? If n or m are not number data type, what will happen? If n or m are zero, I need to return zero and not -1.
+// Explain - If I am given a chocolate bar of n by m units in size, how many breaks for the chocolate bar to be broken into 1 unit square pieces?
+//            Because I wouldn't break the last square, this looks like it is just the area of the bar minus the last square.
+
+/* Some example cases to spot the pattern:
+
+ 2 x 1  2 x 2   2 x 3     2 x 4       2 x 5         2 x 6           2 x 7
+   _     _ _    _ _ _    _ _ _ _    _ _ _ _ _    _ _ _ _ _ _    _ _ _ _ _ _ _
+  |_|   |_|_|  |_|_|_|  |_|_|_|_|  |_|_|_|_|_|  |_|_|_|_|_|_|  |_|_|_|_|_|_|_|
+  |_|   |_|_|  |_|_|_|  |_|_|_|_|  |_|_|_|_|_|  |_|_|_|_|_|_|  |_|_|_|_|_|_|_|
+
+   1      3       5         7           9             11              13
+
+Minimum breaks = ( rows x columns ) - 1
+   */
+
+// Pos Self Talk: :)
+// Translate:
+function breakChocolate(n, m) {
+  // guard against input that isn't a number
+  if (typeof n !== 'number' || typeof m !== 'number') return 0;
+  // because NaN is of type 'number', specifically check for NaN
+  // (technically NaN > 0 will evaluate to false but it is best to be explicit)
+  if (Number.isNaN(n) || Number.isNaN(m)) return 0;
+  // If n or m are zero, I need to return zero and not -1.
+  if (n > 0 && m > 0) {
+    // number of breaks is the area of the bar minus the last square
+    return (n * m - 1);
+  } else {
+    return 0
+  }
 }
