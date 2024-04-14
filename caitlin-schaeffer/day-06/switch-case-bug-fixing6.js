60d7abeac6f461c15cb1d493feaf2e965988edab
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-04-13 20:38:21 -0400

    Caitlin week 2 feedback

--- caitlin-schaeffer/day-06/switch-case-bug-fixing6.js
+++ caitlin-schaeffer/day-06/switch-case-bug-fixing6.js
@@ -1,7 +1,7 @@
 /*
 Params: Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
 Return: The evaluated string as a number
-Problems: The eval() function evaluates JavaScript code represented as a string and returns its 
+Problems: The eval() function evaluates JavaScript code represented as a string and returns its
 completion value. The source is parsed as a script. However it says to never use this!
 Solution: add breaks
 Given Code:
@@ -17,28 +17,44 @@ function evalObject(value){
   }
   return result;
 }
-*/ 
+*/
 
-// I think maybe it's just missing break statements? I think right now the code may not be evaluting 
+// I think maybe it's just missing break statements? I think right now the code may not be evaluating
 // everything as it's own function? will try to add break statements to see if that works:
 // breaks basically push it to the next statement: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/break
 // I think the other option would be to take away the equal sign after result and replace with a colon
 // or to simplify the code to like '+': (a, b) = a + b,?
 
-function evalObject(value){
-    var result = 0;
-    switch(value.operation){
-      case'+': result = value.a + value.b;
+function evalObject(value) {
+  var result = 0;
+  switch (value.operation) {
+    case '+': result = value.a + value.b;
       break;
-      case'-': result = value.a - value.b;
+    case '-': result = value.a - value.b;
       break;
-      case'/': result = value.a / value.b;
+    case '/': result = value.a / value.b;
       break;
-      case'*': result = value.a * value.b;
+    case '*': result = value.a * value.b;
       break;
-      case'%': result = value.a % value.b;
+    case '%': result = value.a % value.b;
       break;
-      case'^': result = Math.pow(value.a, value.b);
-    }
-    return result;
+    case '^': result = Math.pow(value.a, value.b);
   }
+  return result;
+}
+
+/*
+  Ember's Feedback:
+  - Great job! Killing it!
+*/
+// Alternate solution
+function evalObject(value) {
+  switch (value.operation) {
+    case '+': return value.a + value.b;
+    case '-': return value.a - value.b;
+    case '/': return value.a / value.b;
+    case '*': return value.a * value.b;
+    case '%': return value.a % value.b;
+    case '^': return Math.pow(value.a, value.b);
+  }
+}
--- caitlin-schaeffer/day-07/absent-vowel.js
+++ caitlin-schaeffer/day-07/absent-vowel.js
@@ -5,7 +5,7 @@ E has an index of 1,
 I has an index of 2,
 O has an index of 3,
 U has an index of 4.
-Notes: There is no need for string validation and every sentence given will contain all vowels but one. 
+Notes: There is no need for string validation and every sentence given will contain all vowels but one.
 Also, you won't need to worry about capitals.
 Return: a vowel
 Solutions: run a for loop to check if vowel is in the string
@@ -17,7 +17,7 @@ function absentVowel(x){
 */
 
 // I want to write a code that runs through every single letter of a given sentence and returns if something is missing
-// If this doesn't work, just make an if else running through every single vowel 
+// If this doesn't work, just make an if else running through every single vowel
 // some comments left by Ember in explanation
 // you can rename stuff in their given code!
 
@@ -26,7 +26,7 @@ function absentVowel(inputString) {
   // I want to return a number associated with each vowel,
   // the number is associated with the vowels index in this array
   const vowels = ['a','e','i', 'o', 'u'];
-  
+
   // I will iterate over my vowels array to search my input string for each vowel
   // took out the 'i' & 'j'
   for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex++) {
@@ -36,9 +36,9 @@ function absentVowel(inputString) {
 
     // I will iterate over the input string to search for the current vowel
     for (let inputStringIndex = 0; inputStringIndex < inputString.length; inputStringIndex++) {
-      
+
       let currentCharacter = inputString[inputStringIndex]
-      
+
       if (currentVowel === currentCharacter) {
         found = true;
         // break out of this for loop if I found the vowel
@@ -54,6 +54,15 @@ function absentVowel(inputString) {
   }
 }
 
+/*
+  Ember's Feedback:
+  - Good job!
+  - put the old code into a notes.js file
+*/
+// Alternate solution
+function absentVowel(x) {
+    return ["a", "e", "i", "o", "u"].findIndex(vowel => !x.includes(vowel));
+}
 
 
 
@@ -64,7 +73,7 @@ function absentVowel(inputString) {
 
 
 /*
-Broken code/pre-used code: 
+Broken code/pre-used code:
 
 function absentVowel(x){
     let absentVowel = [];
@@ -76,7 +85,7 @@ function absentVowel(x){
       vowels[4] = 'u';
 //all I need to do is get these vowel indexes to return a number!
       for (let i = 0; i < vowels.length; i ++) {
-          let found = false; 
+          let found = false;
           for (let j = 0; j < absentVowel.length; j ++) {
               if (vowels[i] === absentVowel[j]) {
                   found = true;
@@ -99,7 +108,7 @@ function absentVowel(x){
     vowels[3] = 'o';
     vowels[4] = 'u';
     for (let i = 0; i < vowels.length; i ++) {
-        let found = false; 
+        let found = false;
         for (let j = 0; j < x.length; j ++) {
             if (vowels[i] === x[j]) {
                 found = true;
@@ -123,7 +132,7 @@ function absentVowel(x){
     const vowels = ['a', 'e', 'i', 'o', 'u']
     let absentVowel = x;
     for (let i =0; i < vowels.length; i ++) {
-        let found = false; 
+        let found = false;
         for (let j = 0; j < x.length; j ++) {
             if (vowels[i] === x[j]) {
                 found = true;
