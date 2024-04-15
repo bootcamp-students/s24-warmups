2c2c756bb9dd84c930e980d0d4a17591f949245f
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-04-15 14:07:32 -0400

    Update tip-calculator.js

--- jay-miller/day-1/tip-calculator.js
+++ jay-miller/day-1/tip-calculator.js
@@ -1,1 +1,22 @@
-
+function calculateTip(amount, rating) {
+//Declare ratings 
+let rate = rating.toLowerCase();
+if (rate === "excellent"){
+  return Math.ceil(amount*20/100);
+}
+if (rate === "great"){
+  return Math.ceil(amount*15/100);
+}
+if (rate === "good"){
+  return Math.ceil(amount*10/100);
+}
+if (rate === "poor"){
+  return Math.ceil(amount*5/100);
+}
+if (rate === "terrible"){
+  return Math.ceil(amount*0/100);
+}
+  else{
+    return "Rating not recognised";
+  }
+}
