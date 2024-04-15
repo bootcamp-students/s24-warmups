61013991c841a5a3933c20d8de4e97b7e0edfe1a
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-04-15 13:15:10 -0400

    Update Day-8-make-every-letter-after-x-caps.js

--- jay-miller/Day-8-make-every-letter-after-x-caps.js
+++ jay-miller/Day-8-make-every-letter-after-x-caps.js
@@ -1,1 +1,9 @@
-
+var makeEveryLetterAfterXCaps = function (str, letter) {
+  let letterArray = str.split('');
+  for (let i = 0; i < letterArray.length -1; i++) {
+    if (letter === letterArray[i]) {
+      letterArray[i + 1] = letterArray[i + 1].toUpperCase();
+    }
+   }
+   return letterArray.join('');
+};
