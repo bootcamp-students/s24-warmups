5d7eb8ad2337ff392daf3b6e3f3932dc706cc677
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-15 09:51:41 -0400

    working on dna still

--- caitlin-schaeffer/day-11/complementary-dna.js
+++ caitlin-schaeffer/day-11/complementary-dna.js
@@ -1,9 +1,34 @@
 /*
-Instructions: 
-Params: 
-Return: 
+Instructions: In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+Your function receives one side of the DNA (string, except for Haskell); you need to return the
+other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+Params: given string
+Return: string
 Solution: 
 
 Given Code: 
-
+function dnaStrand(dna){
+  return ""
+}
 */
+
+
+function dnaStrand(dna){
+    // this will split the dna string into individual letters 
+    let newArray = dna.split('')
+// write an if/else statement 
+    let newDNA(newArray) {
+    if ('a') {
+        return 't'
+    } else if ('t') {
+        return 'a'
+    } else if ('c') {
+        return 'g'
+    } else if ('g') {
+        return 'c'
+    }
+}
+    // now put all of the letters together
+    return newDNA.concat()
+
+  }
