46b52964f616374cea8ffbc1158842afcd9d8933
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-04-15 14:21:34 -0400

    Update shortest-word.js

--- jay-miller/day-11/shortest-word.js
+++ jay-miller/day-11/shortest-word.js
@@ -1,1 +1,8 @@
-
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
+//use .split
+//Find shortest word with .length
+//Use currentWord to see if it's greater than shortestWord
+//Then return shortestWord
+//used .map on .split
