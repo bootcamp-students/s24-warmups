3a499cd6fe1b7ccf51bea011bbfe43a2c79dad10
Owen Coombs <53359277+OwenCoombs@users.noreply.github.com>
2024-04-14 20:01:25 -0400

    Create every-letter-caps.js

---
+++ owen-coombs/day-08/every-letter-caps.js
@@ -0,0 +1,18 @@
+//return should be uppercase letters after x
+
+ const makeEveryLetterAfterXCaps = function (str, letter) {
+    let result = "";
+    let capitalizeNext = false;
+      
+    for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i];
+    
+    if (capitalizeNext) {
+      currentChar = currentChar.toUpperCase();
+    }
+      capitalizeNext = currentChar === letter;
+      result += currentChar;
+    }
+      
+      return result;
+    };
