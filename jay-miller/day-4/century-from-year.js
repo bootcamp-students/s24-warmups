33f80f281facd8a2c5d783d4eb10d93f7bd0bf84
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-04-15 14:10:12 -0400

    Update century-from-year.js

--- jay-miller/day-4/century-from-year.js
+++ jay-miller/day-4/century-from-year.js
@@ -1,1 +1,7 @@
-
+function century(year) {
+  // Finish this :)
+  //given year for century
+  //first century 1-100 second sentury 101-200
+  // Divid year by 100 to get century
+  return Math.ceil(year/100);
+}
