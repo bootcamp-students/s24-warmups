64fc0c1bbe0e0228584ae7f3113d8b0e27f7383c
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-04-15 14:12:07 -0400

    Update debug-the-function-EASY.js

--- jay-miller/day-7/debug-the-function-EASY.js
+++ jay-miller/day-7/debug-the-function-EASY.js
@@ -1,1 +1,11 @@
+function multi(arr) {
+  return arr.reduce((acc, curr) => acc * curr, 1);
+}
 
+function add(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
