dd4ac7bb8f12e55885524594bc2f9e0527f8f574
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-04-15 14:11:45 -0400

    Update absent-vowel.js

--- jay-miller/day-7/absent-vowel.js
+++ jay-miller/day-7/absent-vowel.js
@@ -1,1 +1,14 @@
+const absentVowel = (string) => {
+  const vowelArray = ['a', 'e', 'i', 'o', 'u'];
+  let vowelObj = {};
 
+  for (let i = 0; i < string.length; i++) {
+    vowelObj[string[i]] = true;
+  }
+
+  for (let j = 0; j < vowelArray.length; j++) {
+      if(vowelObj[vowelArray[j]] === undefined) {
+          return j;
+      }
+  }
+}
