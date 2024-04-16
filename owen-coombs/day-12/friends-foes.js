93c7c798ccf67bd806ba279f4d960c0c2ac4567f
Owen Coombs <53359277+OwenCoombs@users.noreply.github.com>
2024-04-16 08:56:10 -0400

    Create friends-foes.js

---
+++ owen-coombs/day-12/friends-foes.js
@@ -0,0 +1,10 @@
+function friend(friends){
+  let friend = friends.filter(name => name.length === 4)
+  return friend
+  console.log('names:', names)
+ 
+}
+
+
+// use filter method
+// return friend
