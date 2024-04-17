da9d868347a29b5e9d340884d5ea50b49e051af9
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-04-17 09:46:11 -0400

    Update list-filtering.js

--- jay-miller/day-13/list-filtering.js
+++ jay-miller/day-13/list-filtering.js
@@ -1,1 +1,10 @@
-
+function filter_list(l) {
+  // Create a new array and filter out strings
+return l.filter(v => typeof v =="number");
+  // .filter will return a new array
+  // Give .filter test function
+  // Test function should test each element
+  // that it is not a string
+  // Our test function will return true if not a string
+  
+}
