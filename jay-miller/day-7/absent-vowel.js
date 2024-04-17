e4187a75498976688c32b8378319d55c330b1f20
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-04-17 09:41:57 -0400

    Jay week 2 feedback

--- jay-miller/day-7/absent-vowel.js
+++ jay-miller/day-7/absent-vowel.js
@@ -7,8 +7,19 @@ const absentVowel = (string) => {
   }
 
   for (let j = 0; j < vowelArray.length; j++) {
-      if(vowelObj[vowelArray[j]] === undefined) {
-          return j;
-      }
+    if (vowelObj[vowelArray[j]] === undefined) {
+      return j;
+    }
   }
+}
+/*
+  Ember's Feedback:
+  - Very interesting approach!
+  - Good job getting to a solution
+  - Great foray into objects :D
+  - Add some pseudo code, it is what will make this click and what will get your hired!
+*/
+// Alternate solution
+function absentVowel(x) {
+  return ["a", "e", "i", "o", "u"].findIndex(vowel => !x.includes(vowel));
 }
