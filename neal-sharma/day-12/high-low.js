8b7e436b821ba0e010910951381190c2ddcd0e06
KumarG23 <sharma.neal@yahoo.com>
2024-04-17 07:47:19 -0400

    typo

--- neal-sharma/day-12/high-low.js
+++ neal-sharma/day-12/high-low.js
@@ -2,7 +2,7 @@
 parameters- take a string of numbers and return the highest and lowers. in that order
 return- highest and lowest in string.
 example- '1 2 3 4 5' return '5 1'
-concers- turning my pseudocode into real code. fully understanding math.min and math.max etc.
+concerns- turning my pseudocode into real code. fully understanding math.min and math.max etc.
 explain- biggest issue was syntax. using string interpolation i plugged in math.min and math.max
 positive self talk- practice puts brains in your muscles
 
