c0537697d6432c798f371fb57a55d24aafd8c4b8
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-04-16 23:19:24 -0400

    Owen week 2 feedback

--- owen-coombs/day-09/multidimensional-array-day-09.js
+++ owen-coombs/day-09/multidimensional-array-day-09.js
@@ -1,14 +1,19 @@
-//parameters-array,indexes(array of numbers number [])
-//return-an element from array parameter
-//example-([[1,2],[3,4]]),[0,0])
-//concerns-improper paramater data type
+// parameters-array,indexes(array of numbers number [])
+// return-an element from array parameter
+// example-([[1,2],[3,4]]),[0,0])
+// concerns-improper parameter data type
 // return index
-
-
-function getElement(array, indexes){
+function getElement(array, indexes) {
   let currentIndex = array
   for (const index of indexes) {
     currentIndex = currentIndex[index];
   }
   return currentIndex;
 }
+/*
+  Ember's Feedback:
+  - We went over this in class just LMK if it is still confusing
+  - indexes is a list of positions, so we iterate on that list and drill into the input
+    array to find the thing located by the position values in the indexes input. This kata is
+    predicated on the assumption that the content of array will always match the positions provided by indexes.
+*/
