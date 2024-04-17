9375515a0c5e55a5ded483821e81b2f8fcafe133
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-04-17 07:55:35 -0400

    Delete jay-miller/day-12/Friend-Or-Foe.js

--- jay-miller/day-12/Friend-Or-Foe.js
+++
@@ -1,6 +0,0 @@
-function friend(friends){
-//Use .filter to get names
-//if name.length === 4: Passed
-//if name.length doesnt === 4: Failed
-return friends.filter(function (item) { return item.length === 4 });  
-}
