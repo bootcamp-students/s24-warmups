f72644725904eb4866bb53d9a275bb0440400442
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-04-18 10:16:40 -0400

    Delete jay-miller/day-11 directory

--- jay-miller/day-11/shortest-word.js
+++
@@ -1,8 +0,0 @@
-function findShort(s){
-    return Math.min(...s.split(" ").map (s => s.length));
-}
-//use .split
-//Find shortest word with .length
-//Use currentWord to see if it's greater than shortestWord
-//Then return shortestWord
-//used .map on .split
