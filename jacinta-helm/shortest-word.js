b8ab3d7c3bbcdec15676af8a5f33911e09e2f858
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-04-21 08:51:57 -0400

    Update shortest-word.js

--- jacinta-helm/shortest-word.js
+++ jacinta-helm/shortest-word.js
@@ -1,15 +1,34 @@
-
 function findShort(s){
-  console.log('s', s);
-  const words = s.split(' ')
+  console.log('s',s);
+  const words = s.split(' ');
   console.log('words', words);
-  const shortestWord = words.reduce((acc, cur) => {
-    console.log('acc, cur', acc, cur)
-    return acc < cur.length ? acc : cur.length
-  }, 30000)
-    console.log('shortestWord: ', shortestWord);
-  return shortestWord
-} 
+  let shortestWord = words.reduce((acc, cur) => {
+    return cur.length < acc.length ? cur : acc;
+  });
+  return shortestWord.length; 
+ 
+}
+
+
+//P find the shorest word
+// R my function should return the shortest word.
+//Example ['I eat bananas'], 1;
+//Concerns use the wrong function and confuse the computer 
+//Explain, I will split the entire string to break up every word. and then reduce the string, iterating over each word
+//using acc and cur as values. if the current value is less than acc then it tracks that length of the word until it reaches the end of the string. 
+
+
+// function findShort(s){
+//   console.log('s', s);
+//   const words = s.split(' ')
+//   console.log('words', words);
+//   const shortestWord = words.reduce((acc, cur) => {
+//     console.log('acc, cur', acc, cur)
+//     return acc < cur.length ? acc : cur.length
+//   }, 30000)
+//     console.log('shortestWord: ', shortestWord);
+//   return shortestWord
+// } 
 
 //P find a function that finds the shortest word in a string.
 //R my function should include the shortest word.
