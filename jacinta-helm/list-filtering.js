f8f92e26f5001bc4cc85328e7d2345aa821e68eb
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-04-21 11:58:21 -0400

    Update list-filtering.js

--- jacinta-helm/list-filtering.js
+++ jacinta-helm/list-filtering.js
@@ -1,1 +1,14 @@
+function filter_list(l) {
+  console.log('l', l);
+  return l.filter(item => typeof item !== 'string');
+
+}
+
+
+//P create function that returns a new list with the strings filtered out. 
+//R my function should return a new list without the strings 
+//Example ([1, 2, 'a', 'b']) = [1, 2]
+//Concerns there are no strings to remove.
+//Explain, i can use a filter method and type of method to filter out types of strings
+
 
