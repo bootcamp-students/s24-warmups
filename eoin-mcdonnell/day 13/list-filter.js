f2bacf14b35c600055669bc0620f990536561252
oldmcdonnell <oldmcdonnell@gmail.com>
2024-04-21 20:10:38 -0400

    list filter

--- eoin-mcdonnell/day 13/list-filter.js
+++ eoin-mcdonnell/day 13/list-filter.js
@@ -1,1 +1,5 @@
-filterStr
+//l . filter with iterate ove the array and the function with return variable v with a type of exactly equal to number
+
+function filter_list(l) {
+    return l.filter(function(v) {return typeof v === 'number'})
+  }
