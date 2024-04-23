b71c03acafb3654a19891c27b25617ffbf120273
nwils000 <nwils000@gmail.com>
2024-04-23 08:18:17 -0400

    finished todays katas

---
+++ nathan-wilson/day-17/create-phone-number.js
@@ -0,0 +1,20 @@
+function createPhoneNumber(numbers) {
+  let firstThreeNumbers = numbers.slice(0, 3).join('');
+  let secondThreeNumbers = numbers.slice(3, 6).join('');
+  let four = numbers.slice(6, 10).join('');
+
+  return `(${firstThreeNumbers}) ${secondThreeNumbers}-${four}`;
+}
+
+// an array of integers
+
+// return a string with those integers in the form of a string
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890"
+
+// I could format the string wrong, or have some type errors throughout it
+
+// Take the array of numbers, break them up into array of 3, 3, and 4.
+// Then join them all seperately in a template literal
+
+// I believe in myself
