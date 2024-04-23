09c1e31a72edea0bc60bbb622ad80f05586d6f37
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-23 09:42:08 -0400

    set up phone numbers

--- caitlin-schaeffer/day-17/phone-number.js
+++ caitlin-schaeffer/day-17/phone-number.js
@@ -1,12 +1,25 @@
 /*
-Instructions: 
-Example: 
-Params: 
-Return: 
+Instructions: Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of 
+those numbers in the form of a phone number.
+Example: createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+or
+    assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+Params: given an array of integers 
+Return: Must return "(123) 456-7890", must return it in that format with spaces 
 Solution: 
 
 Given Code: 
+function createPhoneNumber(numbers){
+  
+}
 */
+
+// Basically you need to randomize (or not randomize) just take an array of integers into a string 
+
+
+function createPhoneNumber(numbers){
+  
+}
 
 /* Ember says:
 '('+ arr[0] + arr[1] + arr[2]+') ' + 
