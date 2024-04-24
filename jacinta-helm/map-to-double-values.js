afcd76d726d745b9d844ee6a310240fc0e17da25
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-04-24 08:39:57 -0400

    Update map-to-double-values.js

--- jacinta-helm/map-to-double-values.js
+++ jacinta-helm/map-to-double-values.js
@@ -1,1 +1,15 @@
+function double(array) {
+    // Use array.map() to return a new array with each value twice
+    // as large as the corresponding value in the passed in array.
+  console.log('array', array);
+//   let numbers = []
+//   let numbers = doubledNumbers.map(doubledNumber => number * 2)
+  return array.map(element => element * 2)
+  
+}
 
+//P create a function that doubles the numbers in the array
+// R my function should return the numbers in the array doubled in the answer. 
+//Example [1,2,3,4,5] is = [2,4,6,8,10]);
+// C concern would be syntax or not remebering how to get the answer.
+//Explain, use the map over function to make the math and multiply the element by 2. 
