37d4e3ee679cda5d94f96ebf08d0bad1783affe5
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-24 09:04:26 -0400

    done with the finding odd

--- caitlin-schaeffer/day-16/find-the-odd.js
+++ caitlin-schaeffer/day-16/find-the-odd.js
@@ -13,7 +13,45 @@ function findOdd(A) {
 }
 */
 
+// The find() method of Array instances returns the first element in the provided array that satisfies the
+// provided testing function. 
 
+/* So we're going to first attach the find method, and then in the find function we are going to filter the elements so 
+that if the modulo is 2 they cannot have a remainder of 0 */
+
+function findOdd(A) {
+  return A.find(int => A.filter(el => el === int).length % 2 !== 0); 
+  }
+
+
+  /*
+  Code work out:
+  the given (A), will return an array of integers 
+  SO: 
+1. Count each item in an array
+2. identify any repeated integer
+3. if integer is repeated an odd number of times / return that integer
+4. else / return integer that appears once 
+5. return answer 
+
+
+
+let elementsIHaveSeen = {}
+A.forEach (element => {
+    if(elementsIHaveSeen.hasOwnProperty(element)){
+        elementsIHaveSeen.hasOwnProperty[element] += 1
+    } else {
+        elementsIHaveSeen[element] = 1
+    }
+}
+for (let element in elementsIHaveSeen ) {
+if (elementsIHaveSeen[element] % 2 !== 0) return Number.(element)
+}
+return A.find( int => A.filter(el => el === int).length % 2 !== 0);
+}
+)
+
+Or something like
 
 function findOdd(A) {
     let answer = x 
@@ -30,36 +68,4 @@ function findOdd(A) {
 
     return 
   }
-
-  /*
-  Code work out:
-  the given (A), will return an array of integers 
-  SO: 
-1. Count each item in an array
-2. identify any repeated integer
-3. if integer is repeated an odd number of times / return that integer
-4. else / return integer that appears once 
-5. return answer 
-  */
-
-/*
-Ember:
-return A.find(int => A.filter(el => el === int).length % 2 !== 0); 
-
-OR
-
-let elementsIHaveSeen = {}
-A.forEach (element => {
-    if(elementsIHaveSeen.hasOwnProperty(element)){
-        elementsIHaveSeen.hasOwnProperty[element] += 1
-    } else {
-        elementsIHaveSeen[element] = 1
-    }
-}
-for (let element in elementsIHaveSeen ) {
-if (elementsIHaveSeen[element] % 2 !== 0) return Number.(element)
-}
-return A.find( int => A.filter(el => el === int).length % 2 !== 0);
-}
-)
 */
