ba6f75a194800708f1dc39164a3432a59c45ce84
Cody <c.jmiller17@yahoo.com>
2024-04-24 08:39:39 -0400

    Coments

--- cody-miller/day-18/double-array.js
+++ cody-miller/day-18/double-array.js
@@ -1,3 +1,9 @@
+/*
+The map function can accept equations. Because we want to
+double what is in the array, we simply just times everything 
+by the two.
+*/
+
 function double(array) {
     let twice = array.map((thing) => thing * 2);
     return twice;
