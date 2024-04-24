e8930f0f28dc1b15b61a6256dabf7759072c5bc2
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-24 08:35:18 -0400

    done with react

--- caitlin-schaeffer/day-18/component-types.js
+++ caitlin-schaeffer/day-18/component-types.js
@@ -6,5 +6,19 @@ Return:
 Solution: 
 
 Given Code:
-
+const React = require('react');
+//add Hello, World and Greet components here. refer to description if you are stuck. good luck :)
 */
+
+const React = require('react');
+//add Hello, World and Greet components here. refer to description if you are stuck. good luck :)
+
+const Hello=()=><h1>Hello</h1>;
+
+const World=()=><h2>World!</h2>;
+
+class Greet extends React.Component {
+    render() {
+        return (<div><Hello /><World /></div>)
+    }
+}
