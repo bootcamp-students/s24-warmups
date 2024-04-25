c8f958639256f87cd5103ff0e381b39cb8046f64
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-04-25 08:14:35 -0400

    established folder

---
+++ caitlin-schaeffer/day-19/training-on-stop.js
@@ -0,0 +1,18 @@
+/*
+Instructions: Write a function that takes in a string of one or more words, and returns the same string, but with all words 
+that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters 
+and spaces. Spaces will be included only when more than one word is present.
+Example: "Hey fellow warriors"  --> "Hey wollef sroirraw" ; "This is a test   --> "This is a test" 
+Params: given a string
+Return: a string
+Solution: 
+
+Given Code:
+function spinWords(string){
+  //TODO Have fun :)
+}
+*/
+
+function spinWords(string){
+    //TODO Have fun :)
+  }
