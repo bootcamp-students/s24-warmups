71441c08cb11a44a5c54e766c458fea803d0462d
Owen Coombs <53359277+OwenCoombs@users.noreply.github.com>
2024-04-25 08:44:20 -0400

    Create training-on-reverse.js

---
+++ owen-coombs/day-19/training-on-reverse.js
@@ -0,0 +1,18 @@
+function spinWords(string){
+  const words = string.split(" ");
+  for (let i = 0; i < words.length; i++){
+    
+    if(words[i].length >= 5){
+        words[i] = words[i].split("").reverse().join("");
+  
+    }
+  }
+ 
+  return words.join(" ");
+}
+
+//use split()
+// create var for words = to string
+// use reverse() method 
+// create a for loop
+// return words.join
