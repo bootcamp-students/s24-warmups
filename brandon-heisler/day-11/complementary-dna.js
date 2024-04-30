1432469afe0aa3039ed6d1e5409c41349a15a3a6
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-04-30 14:54:22 -0400

    Brandon week 3 feedback

--- brandon-heisler/day-11/complementary-dna.js
+++ brandon-heisler/day-11/complementary-dna.js
@@ -1,22 +1,22 @@
-function dnaStrand(dna){
+function dnaStrand(dna) {
   const dnaArray = dna.split("")
-  
+
   return dnaArray.map(letter => {
-    if (letter === 'A'){
-     return 'T'
+    if (letter === 'A') {
+      return 'T'
     }
-    else if (letter === 'T'){
+    else if (letter === 'T') {
       return 'A'
     }
-    else if (letter === 'C'){
+    else if (letter === 'C') {
       return 'G'
     }
-    else if (letter === 'G'){
+    else if (letter === 'G') {
       return 'C'
     }
   })
-  .join('')
-  
+    .join('')
+
 }
 
 /*
@@ -24,5 +24,31 @@ i want to take a string, break it apart into letters.
 flip each letter to its pair letter
 join it together into a single string again.
 then return it.
-
 */
+/*
+  Ember's Feedback:
+  - Great job :D
+  - Look at that good good array method
+*/
+// Alternative Solution
+function dnaStrand(dna) {
+  let dnaPairs = {
+    A: 'T',
+    T: 'A',
+    G: 'C',
+    C: 'G'
+  }
+  return dna.split('').map(letter => dnaPairs[letter]).join('');
+}
+
+function DNAStrand(dna) {
+  let dnaRelationships = new Map([
+    ['A', 'T'],
+    ['T', 'A'],
+    ['C', 'G'],
+    ['G', 'C']
+  ])
+  return Array.prototype.map.call(dna, letter => dnaRelationships.get(letter) ?? '').join('')
+}
+// because strings are "array like" you can call array methods on them in this
+// way using .call
--- brandon-heisler/day-14/two-to-one.js
+++ brandon-heisler/day-14/two-to-one.js
@@ -1,6 +1,6 @@
-// Ember, I had issues copy and pasting this, i tried to spruce it up before submitting, let me know if it doesnt translate well. have a good one.
+// Ember, I had issues copy and pasting this, i tried to spruce it up before submitting, let me know if it doesn't translate well. have a good one.
 function longest(s1, s2) {
- "use strict";
+  "use strict";
 
   let concatString = (s1 + s2)
 
@@ -8,18 +8,46 @@ function longest(s1, s2) {
 
   lettersArray.sort()
 
-  function removeDupes(data){
+  function removeDupes(data) {
     return data.filter((value, index) => data.indexOf(value) === index);
   }
+
   let resultsArray = removeDupes(lettersArray)
   return resultsArray.join('')
 }
 
 /*
 i have 2 strings given to me and need to return a sorted string with only 1 of each letter.
-strings are immutable so i should combine them into an array, use the array.sort method 
+strings are immutable so i should combine them into an array, use the array.sort method
 and join the array back into a string and return it.
 ive joined strings, split into an array by letter and sorted into order.
-now i need go go over each index once. if the letter isnt in a new array, add the letter.
+now i need go go over each index once. if the letter isn't in a new array, add the letter.
 join the results together and success.
 */
+/*
+  Ember's Feedback:
+  - Look good! no worries here, looks normal.
+  - great job on that filter method, very good :D
+  - the remove dupes function is kind of unneeded, you just call that filter directly on letters Array instead
+*/
+// Alternative Solution
+function longest(s1, s2) {
+  return Array.from(new Set(s1 + s2)).sort().join('');
+}
+
+function longest(s1, s2) {
+  let newStr = s1.concat(s2);
+  let newArr = newStr.split("");
+  let resultArr = [];
+
+  const addUniqueValuesToArray = (accumulator, currentValue) => {
+    if (!accumulator.includes(currentValue)) {
+      accumulator.push(currentValue)
+    }
+    return accumulator
+  }
+
+  newArr.reduce(addUniqueValuesToArray, resultArr)
+  let resultStr = resultArr.sort().join("");
+  return resultStr;
+}
