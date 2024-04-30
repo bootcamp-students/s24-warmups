4c23bb89c350ff7b19858ec7e352a29535332658
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-04-30 00:49:27 -0400

    Jacinta week 3 feedback

--- jacinta-helm/day-12/friend-foe.js
+++ jacinta-helm/day-12/friend-foe.js
@@ -1,14 +1,20 @@
-function friend(friends){
-//   console.log(friends)
+function friend(friends) {
   const myFriend = friends.filter((friend) => friend.length === 4)
-  // console.log(myFriend);
   return myFriend
 }
 
-//P create a function that filters a list of strings and returns a list with only my friends name. 
+//P create a function that filters a list of strings and returns a list with only my friends name.
 //R my function should return my friends name that has 4 letters in it. return value array of strings
 //Example (["ryan", "kieran", "mark"]), ["ryan", "mark"]
 //Concerns- what if friends is an empty array.
-//Explain- I will creat a variable with the list of everybodys name. creat a function using the filter method in it.
-//in that function use friend.length to show the vakue of letters in each name and use an arrow function and use the 
-//strictly equal to operator to 4 to find my friends. Create a new array, using .filter returns a new array for free. 
+//Explain- I will create a variable with the list of everybody's name. create a function using the filter method in it.
+//in that function use friend.length to show the value of letters in each name and use an arrow function and use the
+//strictly equal to operator to 4 to find my friends. Create a new array, using .filter returns a new array for free.
+
+/*
+  Ember's Feedback:
+  - Good job! Great use of an inline function there, no return keyword needed :D
+  - Only thing I would say is myFriend could be better named as myFriends, filteredFriends, etc.
+*/
+// Alternative Solution
+let filteredFriends = (friends) => friends.filter((friend) => friend.length === 4)
