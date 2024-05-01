3c048b3fa11287c057b263062acbfe6c2cc26b61
Cody <c.jmiller17@yahoo.com>
2024-05-01 10:36:32 -0400

    New folder is being tracked now

---
+++ cody-miller/day-23/roman-numeral.js
@@ -0,0 +1,32 @@
+function solution(number) {
+  
+    let roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let str = "";
+
+  for (let i of Object.keys(roman)) {
+    let q = Math.floor(num / roman[i]);
+    num -= q * roman[i];
+    str += i.repeat(q);
+  }
+
+  return str;
+}
+
+/*
+This should be very similar to our getCurrentTime Kata in that
+we have to divide by a larger number, then pass it along to the next divisor.
+*/
