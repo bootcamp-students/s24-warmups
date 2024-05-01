84eb98d93732ce9a9e9d07ae857675f6b5d6f20b
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-01 16:27:34 -0400

    Eoin week 3 feedback

--- eoin-mcdonnell/day-12/highest-and-lowest.js
+++ eoin-mcdonnell/day-12/highest-and-lowest.js
@@ -1,14 +1,47 @@
 'use strict'
-//map is going to iterateo over the array
-//math .min and math .max are goingto find the smallest and largest numbers  
-  
+//map is going to iterate over the array
+//math .min and math .max are going to find the smallest and largest numbers
+
 function highAndLow(numbers) {
   let newNum = numbers.split(' ').map(Number);
-  console.log(newNum)
   const max = Math.max(...newNum);
-  console.log(max)
   const min = Math.min(...newNum);
-  console.log(min)
   return `${max} ${min}`;
 }
+/*
+  Ember's Feedback:
+  - Hey! Good job! :D
+  - Love the use of the rest operator to unpack the array and
+    using the template to turn those number values into a string :D
+*/
+// Alternative Solution
+// Sort
+function highAndLow(numbers) {
+  "use strict"
+  let order = numbers.split(' ').map(Number)
+  order.sort((a, b) => a - b)
+  let answer = `${order.at(-1)} ${order.at(0)}`
+  return answer
+}
 
+// Slightly different reduce where you run it just once
+function highAndLow(numbers) {
+  "use strict"
+  let numbersList = numbers.split(' ').map(Number)
+  let [highestNumber, lowestNumber] = numbersList.reduce((acc, cur) => {
+    if (cur > acc[0]) {
+      acc[0] = cur
+    }
+    if (cur < acc[1]) {
+      acc[1] = cur
+    }
+    return acc
+  }, [-Infinity, Infinity])
+  return `${highestNumber} ${lowestNumber}`
+}
+
+// Min Max
+function highAndLow(numbers) {
+  let numArr = numbers.split(' ')
+  return `${Math.max(...numArr)} ${Math.min(...numArr)}`
+}
