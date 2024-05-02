80dd296f4d6565ed854a6334724bd2eaa13c44a2
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-02 09:21:43 -0400

    still working on day 23

--- caitlin-schaeffer/day-23/roman-numerals.js
+++ caitlin-schaeffer/day-23/roman-numerals.js
@@ -9,6 +9,7 @@ Examples: 1990 is rendered: 1000=M + 900=CM + 90=XC; resulting in MCMXC.
 1666 --> "MDCLXVI"
 Params: given integer
 Return: string value of integer
+Note: If the number is between 900 and 1000, it is the 1 unit minus the 10 unit, if the number is between 500 and 900, it is the 5 unit plus the 1 unit
 Solution: 
 
 Given Code:
@@ -18,7 +19,6 @@ function solution(number){
 
 
 */
-
 
 // logic is almost done on i!!!
 
@@ -87,7 +87,7 @@ function solution(number){
      let vCount = Math.floor(totalMinusX/5);
      let totalMinusV = totalMinusX % 5;
       //returning letter V
-    if (vCount > 0) {
+    if (vCount <= 3) {
         vCount = 'V'.repeat(vCount)
     } else if (vCount > 3) {
         false
@@ -114,3 +114,79 @@ function solution(number){
   return mCount + dCount + cCount + lCount + xCount + vCount + iCount
 
 }
+
+/*
+This was Embers Code:
+function solution(number) {
+    // convert the number to a roman numeral
+    let numTotal = number;
+    let numeralsReturn = "";
+
+    // thousands logic
+    let thousands = Math.floor(number / 1_000);
+    // let thousandsRemainder =
+    numeralsReturn += "M".repeat(thousands);
+    numTotal -= thousands * 1_000;
+
+    // 500s logic
+    let fiveHundredRemainder = numTotal % 500
+    let fiveHundreds = Math.floor(numTotal / 500);
+    if (fiveHundreds > 0 && fiveHundredRemainder >= 400) {
+        numeralsReturn += 'CM'
+        numTotal -= 900
+    } else {
+        numeralsReturn += "D".repeat(fiveHundreds);
+        numTotal -= fiveHundreds * 500
+    }
+
+    // 100s logic
+    let oneHundreds = Math.floor(numTotal / 100);
+    if (oneHundreds > 3) {
+        numeralsReturn += 'CD'
+    } else {
+        numeralsReturn += "C".repeat(oneHundreds);
+    }
+    numTotal -= oneHundreds * 100;
+
+    // 50s logic
+    let fiftiesRemainder = numTotal % 50;
+    let fifties = Math.floor(numTotal / 50);
+    if (fifties > 0 && fiftiesRemainder >= 40) {
+        numeralsReturn += 'XC'
+        numTotal -= 90;
+    } else {
+        numeralsReturn += "L".repeat(fifties);
+        numTotal -= fifties * 50;
+    }
+
+    // 10s logic
+    let tens = Math.floor(numTotal / 10);
+    if (tens > 3) {
+        numeralsReturn += 'XL'
+    } else {
+        numeralsReturn += "X".repeat(tens);
+    }
+    numTotal -= tens * 10;
+
+    // 5s logic
+    let fivesRemainder = numTotal % 5;
+    let fives = Math.floor(numTotal / 5);
+    if (fives > 0 && fivesRemainder > 3) {
+        numeralsReturn += 'IX'
+        numTotal -= 9;
+    } else {
+        numeralsReturn += "V".repeat(fives);
+        numTotal -= fives * 5;
+    }
+
+    // 1s
+    let ones = Math.floor(numTotal / 1);
+    if (ones === 4) {
+        numeralsReturn += 'IV'
+    } else {
+        numeralsReturn += "I".repeat(ones);
+    }
+
+    return numeralsReturn;
+}
+*/
