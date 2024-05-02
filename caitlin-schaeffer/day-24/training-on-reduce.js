ee2df11da439671f881ef9f1171cec77a8b9e906
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-02 09:04:35 -0400

    filter kata done

---
+++ caitlin-schaeffer/day-24/training-on-reduce.js
@@ -0,0 +1,19 @@
+/*
+Instructions: Make the sum() function return the sum of the values in the passed in array. Your solution must use the reduce() 
+function of the built-in javascript Array object.
+Example: 
+Params: 
+Return: 
+Solution: 
+
+Given Code: 
+function sum(array) {
+  // Use array.reduce() to calculate and return the
+  // sum of the values in array.
+}
+*/
+
+function sum(array) {
+    // Use array.reduce() to calculate and return the
+    // sum of the values in array.
+  }
