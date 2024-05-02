86bb86b880de83c40dc9c98b09abfaf70af3f137
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-02 09:09:03 -0400

    done with reduce kata

--- caitlin-schaeffer/day-24/training-on-reduce.js
+++ caitlin-schaeffer/day-24/training-on-reduce.js
@@ -1,10 +1,13 @@
 /*
-Instructions: Make the sum() function return the sum of the values in the passed in array. Your solution must use the reduce() 
+Instructions: 
+Make the sum() function return the sum of the values in the passed in array. Your solution must use the reduce() 
 function of the built-in javascript Array object.
 Example: 
-Params: 
-Return: 
-Solution: 
+var someNumbers = [1,2,3,4,5,6,7,8,9,10];
+sum(someNumbers); // should return 55
+Params: given array
+Return: return sum of array 
+Solution: just run a reduce where we add the current item to the one before
 
 Given Code: 
 function sum(array) {
@@ -14,6 +17,9 @@ function sum(array) {
 */
 
 function sum(array) {
-    // Use array.reduce() to calculate and return the
-    // sum of the values in array.
+    let sum = array.reduce((accumulator, currentItem) =>
+    accumulator + currentItem
+  )
+  console.log('sum: ', sum)
+    return sum
   }
