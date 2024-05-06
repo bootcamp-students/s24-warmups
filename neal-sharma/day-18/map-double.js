f24d7af1d4f2ee735b4f8fc2713081144019f1c5
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-06 18:48:46 -0400

    Neal week 4 feedback

--- neal-sharma/day-18/map-double.js
+++ neal-sharma/day-18/map-double.js
@@ -9,7 +9,34 @@ translate-
 */
 
 function double(array) {
-    const doubled = array.map((x) => x * 2);
-  console.log(doubled);
+  const doubled = array.map((x) => x * 2);
   return doubled;
+}
+/*
+  Ember's Feedback:
+  - Great job :D
+  - Below I demonstrate what map kind of looks like under the hood
+*/
+// Alternative Solution
+function double(array) {
+
+  // This is what .map is. It is a function that exist on all instances of arrays because of prototype inheritance
+  Array.prototype.customMap = function cutomMap(userFunction) {
+
+    if (typeof userFunction !== 'function') throw `${typeof userFunction} is not a function`
+
+    let returnArray = [];
+
+    // "this" here is a way you can get the value of the thing that the function was called on.
+    // so here, "this" is an array and specifically it is whatever is stored in the array variable we call .customMap on
+    for (let i = 0; i < this.length; i++) {
+      returnArray.push(userFunction(this[i], i, this))
+    }
+
+    return returnArray
+  }
+
+  // customMap works because we added it to the Array prototype above.
+  // When we call customMap we give it a function to run on every element. (element => element * 2)
+  return array.customMap(element => element * 2)
 }
