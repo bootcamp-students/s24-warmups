c7d709c1fed91e4a4505f68651b887b326d06f48
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-05 21:01:00 -0400

    Duke week 4 feedback

--- duke-mcclellan/day-16/find-the-odd-int.js
+++ duke-mcclellan/day-16/find-the-odd-int.js
@@ -1,13 +1,27 @@
-//integer that apears an odd number of times
+//integer that appears an odd number of times
 //return an integer that appears an odd number of times
 //I need to return an integer that appears an odd number of times.
 //adding the functionality that it can't be divided by two. Possibly use .reduce? XOR?
 //Key value pairs
-//We can narrow down our search using the reduce function. the XOR (^) function
-//compares values and will remove pairs when working together with reduce.  
-//The 0 is the initial value that is set for A. 
+//We can narrow down our search using the reduce function. the XOR (^) operator
+//compares values and will remove pairs when working together with reduce.
+//The 0 is the initial value that is set for A.
 
 function findOdd(A) {
-  
-  return A.reduce((a,c)=> a ^ c, 0);
+  return A.reduce((a, c) => a ^ c, 0);
+}
+/*
+  Ember's Feedback:
+  - Great job!
+  - This is really advanced stuff, good job digging into it and trying it out.
+  - In the pseudo code it would be good if you explained what XOR is doing.
+  - For this problem, you are keeping the result of flipping the binary representation of the numbers,
+    at each binary spot where both numbers have a 1 it is flipped to a zero. So basically, because you
+    are flipping things an even number of times it will cancel itself out. The only thing that will remain
+    is the number XOR'ed once.
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_XOR
+*/
+// Alternative Solution
+function findOdd(A) {
+  return A.find(int => A.filter(el => el === int).length % 2 !== 0);
 }
--- duke-mcclellan/day-17/house-numbers-sum.js
+++ duke-mcclellan/day-17/house-numbers-sum.js
@@ -1,21 +1,46 @@
 //parameters- an array for the input
 //return - an integer
-//example - possibly using .reduce method for sum? 
-//concern - choosing the wrong method, incorrect syntax or overcomplicating the problem. 
-//I want to be able to determing the sum of the houses but looks like I need to remove pairs and zeros.
+//example - possibly using .reduce method for sum?
+//concern - choosing the wrong method, incorrect syntax or over complicating the problem.
+//I want to be able to determine the sum of the houses but looks like I need to remove pairs and zeros.
 
 function houseNumbersSum(inputArray) {
-    let sum = 0; // init sum to 0
-    for (let num of inputArray){ 
-      if (num === 0){  
-        break; 
-      }
-      sum += num 
+  let sum = 0; // init sum to 0
+  for (let num of inputArray) {
+    if (num === 0) {
+      break;
     }
-    return sum; 
+    sum += num
   }
-  
-
+  return sum;
+}
+/*
+  Ember's Feedback:
+  - Good job!
+  - Looks like you got a little confused with the prompt, but good solution!
+  - Knowing the break keyword is handy
+*/
+// Alternative Solution
+// I solved this the same except I simply returned with the sum when encountering zero.
+function houseNumbersSum(inputArray) {
+  // iterate over the array, producing a sum, until I encounter a zero
+  let sum = 0;
+  for (let int of inputArray) {
+    if (int === 0) return sum;
+    sum += int
+  }
+  return sum;
+}
+/*
+  PRECEPT
+  params: number[]
+  return: number
+  example: [5, 2, 0, 3] => 7
+  concern: undefined input, empty array input, input has no 0; the description implies these will never happen
+  positive: I have summed an array before, this is just that with a stop condition :D
+  translate: I will iterate over the array and produce an element summation. If I encounter a zero, -
+    I will return from the function as a whole or break the iteration in some way.
+*/
 
 
 
