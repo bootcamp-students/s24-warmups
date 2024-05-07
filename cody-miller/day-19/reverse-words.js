1c4f671b3f993ed3dc4c85ea1e853b1a20a53f7c
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-07 13:43:53 -0400

    Cody week 4 feedback

--- cody-miller/day-19/reverse-words.js
+++ cody-miller/day-19/reverse-words.js
@@ -1,17 +1,15 @@
 function spinWords(string) {
   let splitString = string.split(" ");
-  console.log("1: ", splitString)
 
   let mappedString = splitString.map((x) => {
+
     if (x.length >= 5) {
-      console.log(x)
       return x.split("").reverse().join("");
     }
-    console.log("2: ", x);
+
     return x;
   });
-  console.log("3: ", mappedString);
-  console.log("4: ", mappedString.join(" "));
+
   return mappedString.join(" ");
 }
 
@@ -27,4 +25,38 @@ If the word is longer than 5 letters, it needs to be reversed.
 
 String should be put back together and returned.
   Use join method.
+*/
+
+/*
+  Ember's Feedback:
+  - Great work!
+  - You combined a bunch of small things you did previously into a new problem! :D
+    Be proud of your progress :)
+*/
+// Alternative Solution
+function spinWords(string) {
+  // break the string into an array of words
+  let wordsArray = string?.split(" ");
+  // if the array is empty or wordsArray is undefined, the input was bad
+  if (!wordsArray || !wordsArray.length) return string;
+  // write a function to reverse words
+  const reverseWord = (word) => word.split("").reverse().join("");
+  // figure out if any words are larger than 5 characters and reverse them
+  let spinedWordsArray = wordsArray.map((word) =>
+    word.length >= 5 ? reverseWord(word) : word
+  );
+  // return the mutated data back as a string
+  return spinedWordsArray.join(" ");
+}
+/*
+    PRECEPT
+    params: string
+    return: string
+    example: "Hey fellow warriors" => "Hey wollef sroirraw"
+    concerns: no input, invalid input
+    explain:
+        If I detect a word in the string that is over 5 characters, reverse the word.
+        Return a modified string.
+    positive self talk: I know how to reverse strings already and how to break up strings by words!
+    translate: put my pseudocode into comments and solve the problem with JS!
 */
