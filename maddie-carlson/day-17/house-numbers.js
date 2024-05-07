0f2a8703fd38846fbdbdd9364fd7d0e9f8335b2e
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-07 15:52:04 -0400

    Maddie week 4 feedback

--- maddie-carlson/day-17/house-numbers.js
+++ maddie-carlson/day-17/house-numbers.js
@@ -1,10 +1,10 @@
 function houseNumbersSum(inputArray) {
   //function takes an array of numbers
   //needs to return the sum of all numbers BEFORE a zero is encountered in the array
-  
+
   //to solve, loop over the array, summing elements, and returning when encountering a zero
   let sum = 0
-  
+
   for (let i = 0; i < inputArray.length; i++) {
     if (inputArray[i] !== 0) {
       sum += inputArray[i]
@@ -12,4 +12,13 @@ function houseNumbersSum(inputArray) {
       return sum
     }
   }
+}
+/*
+  Ember's Feedback:
+  - Great job! :D
+*/
+// Alternative Solution (less efficient than your solution though)
+function houseNumbersSum(inputArray) {
+  let indexOf0 = inputArray.indexOf(0);
+  return inputArray.slice(0, indexOf0).reduce((a, c) => a + c, 0);
 }
