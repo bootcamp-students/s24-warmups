5dfb4b24a098ccb06d760829ee367526fc22ed9a
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-07 15:34:12 -0400

    Jacinta week 4 feedback

--- jacinta-helm/day-17/house-number.js
+++ jacinta-helm/day-17/house-number.js
@@ -1,8 +1,7 @@
 
 function houseNumbersSum(inputArray) {
-  //coding and coding..
   let sum = 0;
-  
+
   for (let i = 0; i < inputArray.length; i++) {
     if (inputArray[i] === 0) {
       // If 0 is encountered, it marks the end of house numbers
@@ -10,12 +9,21 @@ function houseNumbersSum(inputArray) {
     }
     sum += inputArray[i];
   }
-  
+
   return sum;
 }
-  
+
 //P I need to create a function that stops adding the numbers together after it comes across a zero.
-//R my function should return a function that stops adding the sums after it comes to a zero in the string. 
-//Example [5, 1, 2, 3, 0, 1, 5, 0, 2]),11
-//C there are no zeros in the string. 
+//R my function should return a number
+//Example [5, 1, 2, 3, 0, 1, 5, 0, 2]), 11
+//C there are no zeros in the string.
 //Explain. I will iterate over the string and if the index is equal to a 0 then stop adding the numbers in the array.
+
+/*
+  Ember's Feedback:
+  - Wonderful :D
+*/
+function houseNumbersSum(inputArray) {
+  let indexOf0 = inputArray.indexOf(0);
+  return inputArray.slice(0, indexOf0).reduce((a, c) => a + c, 0);
+}
--- jacinta-helm/day-19/stop-spinning-my-words.js
+++ jacinta-helm/day-19/stop-spinning-my-words.js
@@ -1,6 +1,6 @@
 function spinWords(string) {
   const words = string.split(' ');
-  console.log('words', words);
+
   for (let i = 0; i < words.length; i++) {
     if (words[i].length >= 5) {
       words[i] = words[i].split('').reverse().join('');
@@ -9,33 +9,47 @@ function spinWords(string) {
   return words.join(' ');
 }
 
-    
-    
-
-//       words[i].split('').reverse().join('');
-  
-//   const bigWord = string.filter((spinWord) => spinWord.length >= 5);
-// //     console.log(bigWord);
-//   function reverse(string)
-
-//   return string.split('').reverse().join('');
-  
-
-//   let spinWords = [' ']
-//   const str = spinWords.split(' ');
-//   const bigWords = .filter((spinWords) => string.length >= 4)
-//   console.log(bigWords);
-//   console.log('string', string);
-//   const spinWords = spinWord.filter(())
-
-
-
-
-//P create a function that creates a function that takes a string with 5 or mords and reverses that string. 
-//R my function should return a function that only reverses a string with 5 or more letters
+//P string of words
+//R string with words reversed if the word is 5 or more characters long
 //Example ("Welcome"), "emocleW"
 //"Hey fellow warriors"), "Hey wollef sroirraw"
-//C a concern could be that there are no words with more than 5 letters in it. 
-//Explain i will split the string to seperate each one. see if the length of words are greater than or equal to 5
+//C a concern could be that there are no words with more than 5 letters in it.
+//Explain i will split the string to separate each one. see if the length of words are greater than or equal to 5
 // an use the reverse method to check for if the word is greater than or equal to 5 letters
 // and finish by joining the strings back together.
+
+/*
+  Ember's Feedback:
+  - wonderful job! :D
+  - You should be really proud! You have taken all these little pieces of logic
+    from all of your katas and put them together into a new solution :D
+*/
+
+
+// Alternative Solution
+function spinWords(string) {
+  // break the string into an array of words
+  let wordsArray = string?.split(" ");
+  // if the array is empty or wordsArray is undefined, the input was bad
+  if (!wordsArray || !wordsArray.length) return string;
+  // write a function to reverse words
+  const reverseWord = (word) => word.split("").reverse().join("");
+  // figure out if any words are larger than 5 characters and reverse them
+  let spinedWordsArray = wordsArray.map((word) =>
+    word.length >= 5 ? reverseWord(word) : word
+  );
+  // return the mutated data back as a string
+  return spinedWordsArray.join(" ");
+}
+/*
+    PRECEPT
+    params: string
+    return: string
+    example: "Hey fellow warriors" => "Hey wollef sroirraw"
+    concerns: no input, invalid input
+    explain:
+        If I detect a word in the string that is over 5 characters, reverse the word.
+        Return a modified string.
+    positive self talk: I know how to reverse strings already and how to break up strings by words!
+    translate: put my pseudocode into comments and solve the problem with JS!
+*/
