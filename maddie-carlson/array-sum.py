e05802cb7bf01f4753a41b56406dad9e60fb0b05
Madeline Carlson <162224573+SpookyLamb@users.noreply.github.com>
2024-05-08 08:06:17 -0400

    Create array-sum.py

---
+++ maddie-carlson/array-sum.py
@@ -0,0 +1,12 @@
+def array_plus_array(arr1,arr2):
+    #takes two arrays of numbers, needs to return the sum of all the elements in the array
+    #to solve, simply loop over both arrays, summing each element, and return
+    
+    sum = 0
+    
+    for i in range(len(arr1)):
+        sum += arr1[i]
+    for i in range(len(arr2)):
+        sum += arr2[i]
+    
+    return sum
