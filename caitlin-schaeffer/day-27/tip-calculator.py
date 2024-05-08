749c0a59da06452c577f28f698046e1444b3eb17
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-08 09:37:54 -0400

    almost done with the tip calculator

--- caitlin-schaeffer/day-27/tip-calculator.py
+++ caitlin-schaeffer/day-27/tip-calculator.py
@@ -1,8 +1,44 @@
-# Instructions: 
-# Examples: 
-# Params: 
+# Instructions: Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+# Terrible: tip 0%
+# Poor: tip 5%
+# Good: tip 10%
+# Great: tip 15%
+# Excellent: tip 20%
+# The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+# "Rating not recognised"
+# Because you're a nice person, you always round up the tip, regardless of the service.
+
+# Examples: 30, "poor" = 2
+# Params: given integer and tip rating 
 # Return: 
-# Concerns: 
+# Concerns: rounding up in python is unknown
 # Solution: 
 
 # Given Code: 
+# def calculate_tip(amount, rating):
+#     #your code here
+
+# Logic:
+# If tip = poor, amount * .5
+# etc
+
+
+def calculate_tip(amount, rating):
+    x = rating.lower()
+    if (x == "terrible"):
+      ter =  amount * 0 
+      return round(ter)
+    elif (x == "poor"):
+       po = amount * .05
+       return round(po)
+    elif (x == "good"):
+       go = amount * .10
+       return round(go)
+    elif (x == "great"):
+       gr = amount * .15
+       return round(gr)
+    elif (x == "excellent"):
+       ex = amount * .2
+       return round(ex)
+    else:
+       return "Rating not recognised"
