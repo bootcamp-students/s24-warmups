2bb86e668751a7ec1768bc8875eb1d0ab6902b85
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-09 22:47:20 -0400

    add regex explanations

--- nathan-wilson/day-21/decode-the-morse-code.js
+++ nathan-wilson/day-21/decode-the-morse-code.js
@@ -29,8 +29,30 @@ decodeMorse = function (morseCode) {
   - Great job!!!
 */
 // Alternative Solution
+// RegEx here matches any number of literal . characters and literal - characters, followed by 0 to 2 spaces
+// That whole match, spaces and all, are replaced by the output from the function I give .replaceAll
+// The function given to replace all takes the capture group, denoted by parenthesis, (i.e. just the dots and dashes)
+// and plugs that into the MORSE_CODE object. It then replaces the whole match with that return value.
 decodeMorse = function (morseCode) {
   return morseCode.trim().replaceAll(/([\.\-]+)\s{0,2}/g, (_, p1) => {
     return MORSE_CODE[p1]
   });
+}
+decodeMorse = function (morseCode) {
+  return morseCode
+    // splits words apart
+    .split('   ')
+    // turn the words into morse code
+    .map(word => word
+      // splits letters in word apart
+      .split(' ')
+      // converts the letters into morse code
+      .map(character => MORSE_CODE[character])
+      // puts letter together
+      .join('')
+    )
+    // puts words back together into a string
+    .join(' ')
+    // removes whitespace at beginning and end of the string
+    .trim();
 }
