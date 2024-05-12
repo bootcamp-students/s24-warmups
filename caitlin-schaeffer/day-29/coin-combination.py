aba26c8f964585a3aa1f0719a0071e9f646d8a75
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-12 18:29:41 -0400

    done with 29

--- caitlin-schaeffer/day-29/coin-combination.py
+++ caitlin-schaeffer/day-29/coin-combination.py
@@ -8,12 +8,23 @@
 # Params: given an integer 
 # Return: returns an array
 # Concerns: naming all of the coins or just using the values in the function
-# Solution: 
+# Solution: Use the double slash (//) to progressively divide it up. 
 
 # Given Code: 
 # def coin_combo(cents):
 #     return
 
-def coin_combo(cents):
-    return
 
+#Notes: 
+# // = Division (floor): divides the first operand by the second
+# so if we use that we can stack up the division basically
+# % = Modulus: returns the remainder when the first operand is divided by the second
+
+def coin_combo(cents):
+    # start with pennies and work up to quarters
+    return [cents % 5, cents % 25 % 10 // 5, cents % 25 // 10, cents // 25]
+
+#pennies = % 5, give the remainder of cents after seeing how much 5 can go into it, and whatever the remainder is is pennies (less than 5)
+# nickles = cents % 25 % 10 // 5, takes away the modulo of 25 (quarters) and 10 (dimes), and then divides by 5 for nickles
+# dimes= follows the same logic as nickles, but just need to take out quarters and then divide by 10
+# quarters then you just need to divide cents by 25
