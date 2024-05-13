7f443a2347d568da22e133fcb2816399f2a2f002
Duke McClellan <162177816+dmcclellan3@users.noreply.github.com>
2024-05-12 22:20:54 -0400

    basic-math.py

--- duke-mcclellan/day-29/basic-math.py
+++ duke-mcclellan/day-29/basic-math.py
@@ -2,7 +2,8 @@
 ##return - string 
 ##example - 1plus2plus3plus4"  --> "10"
 #concerns - learning a new language 
-##explain - 
+##explain - using eval to evaluate the resulting expression stored in the string
+##and replace to remove plus and + and return the string. 
 
 def calculate(s):
     string = s.replace("plus", "+").replace("minus", "-") 
