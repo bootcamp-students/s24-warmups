8170c8671f51645bda8f876e97bd11be3fe80644
Skell87 <136534563+Skell87@users.noreply.github.com>
2024-05-13 08:54:10 -0400

    Update century-from-year.py

--- brandon-heisler/day-31/century-from-year.py
+++ brandon-heisler/day-31/century-from-year.py
@@ -10,3 +10,11 @@ def century(year):
 # return should be an int. 
 # i can divide the year by 100 and if it has no remainder return it.
 # if it cant be divided evenly, i need to round up and add 1 to the result.
+
+# I discovered that I unknowingly was using floor division instead of actual division.
+# this was an awesome AHA moment for me.
+#refactored it would look like...
+#
+# import math
+# def century(year):
+#    return math.ceil(year / 100)
