59a68379aabeef2149c1e455c4cdce2a12518c46
Owen Coombs <53359277+OwenCoombs@users.noreply.github.com>
2024-05-13 08:23:21 -0400

    Delete owen-coombs/day-38 directory

--- owen-coombs/day-38/century-from-year.py
+++
@@ -1,7 +0,0 @@
-import math
-def century(year):
-    # Finish this :)
-    return math.ceil(year / 100)
-
-# use math ceil
-#return mail.ceil and year / 100
--- owen-coombs/day-38/remove-string-spaces.py
+++
@@ -1,5 +0,0 @@
-def no_space(x):
-    #your code here
-    return x.replace(" ", "")
-
-# use replace to remove whitespace
