47c0de51ac4a8eac51c6dc394b3857e56f247400
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-05-13 16:22:49 -0400

    Update Century-From-Year.py

--- jay-miller/Day-31/Century-From-Year.py
+++ jay-miller/Day-31/Century-From-Year.py
@@ -1,1 +1,7 @@
-
+def century(year):
+    if year < 1000:
+        return year // 100 + 1
+    elif year >= 1000 and year % 100 == 0:
+        return year / 100
+    else:
+        return year // 100 + 1
