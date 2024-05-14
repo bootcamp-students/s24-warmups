0b29816b74e0fe48c2f4ee101eee7952a7c73581
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-14 08:30:28 -0400

    done with length

---
+++ caitlin-schaeffer/day-32/easy-sql-ordering.sql
@@ -0,0 +1,15 @@
+-- Instructions: Your task is to sort the information in the provided table 'companies' by number of employees (high to low). 
+--Returned table should be in the same format as provided:
+-- companies table schema:
+-- id
+-- ceo
+-- motto
+-- employees
+
+-- Examples: 
+-- Params: 
+-- Return: 
+-- Concerns: 
+-- Solution: 
+
+-- Given Code: 
