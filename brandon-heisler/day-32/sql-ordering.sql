1935654e0ec048f1336e80f16b7eca0a79d9e072
Skell87 <136534563+Skell87@users.noreply.github.com>
2024-05-14 08:36:06 -0400

    Create sql-ordering.sql

---
+++ brandon-heisler/day-32/sql-ordering.sql
@@ -0,0 +1,16 @@
+SELECT * FROM companies
+ORDER BY employees DESC;
+/*
+  I need to sort information in a table called companies by
+  number of employees, high to low
+  table schema should be:
+  id
+  ceo
+  motto
+  employees
+  
+  im not super sure where to start. maybe with seeing what i have access to.
+  I queried the table i have to see what im working with,
+  i need to just order by one column and by highest to lowest. 
+  i used order by and found DESC to change the direction of the sort
+*/
