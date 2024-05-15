7371cc42336fea51b5d50bd42489530b1c750bb2
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-15 08:48:00 -0400

    done with best sellers

--- caitlin-schaeffer/day-33/best-selling-books.sql
+++ caitlin-schaeffer/day-33/best-selling-books.sql
@@ -1,5 +1,21 @@
--- Instructions: 
--- Example: 
--- Params: 
--- Return: 
--- Solution: 
+/*
+Instructions: You work at a book store. It's the end of the month, and you need to find out the 5 bestselling books at your 
+store. Use a select statement to list names, authors, and number of copies sold of the 5 books which were sold most.
+books table schema
+-name
+-author
+-copies_sold
+Example: 
+Params: 
+Return: 
+Solution: 
+*/
+
+Select 
+    Name,
+    Author,
+    Copies_sold
+FROM    
+    Books
+ORDER BY
+    copies_sold DESC LIMIT 5
