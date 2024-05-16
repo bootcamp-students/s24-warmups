4d54a52c68c4713fefa76421188ce01b060c0b81
Jaymiller2002 <122222452+Jaymiller2002@users.noreply.github.com>
2024-05-16 09:16:05 -0400

    Update Conditional-Count.sql

--- jay-miller/Day-34/Conditional-Count.sql
+++ jay-miller/Day-34/Conditional-Count.sql
@@ -1,1 +1,11 @@
-
+-- Replace with your SQL Query
+SELECT EXTRACT(MONTH FROM payment_date) AS month,
+COUNT(*) AS total_count,
+SUM(amount) AS total_amount,
+COUNT(CASE WHEN(staff_id = 1)THEN amount ELSE null END) AS mike_count,
+SUM(CASE WHEN(staff_id = 1)THEN amount END) AS mike_amount,
+COUNT(CASE WHEN(staff_id = 2)THEN amount ELSE null END) AS jon_count,
+SUM(CASE WHEN(staff_id = 2)THEN amount ELSE 0 END) AS jon_amount
+FROM payment
+GROUP BY month
+ORDER BY month
