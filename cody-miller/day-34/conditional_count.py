2179eb04af30f990ec8b577a0b7e126fe6d9771f
Cody <c.jmiller17@yahoo.com>
2024-05-20 06:40:25 -0400

    Added Cond Count

--- cody-miller/day-34/conditional_count.py
+++ cody-miller/day-34/conditional_count.py
@@ -1,2 +1,12 @@
-Skip
+select 
+extract(MONTH from payment_date) as month,
+count(payment_id) as total_count,
+sum(amount) as total_amount,
+count(CASE WHEN staff_id = 1 THEN 1 END) as mike_count,
+SUM(CASE WHEN staff_id = 1 THEN amount ELSE 0 END) as mike_amount,
+count(CASE WHEN staff_id = 2 THEN 1 END) as jon_count,
+SUM(CASE WHEN staff_id = 2 THEN amount ELSE 0 END) as jon_amount
+from payment
+GROUP BY month
+ORDER BY month;
 
