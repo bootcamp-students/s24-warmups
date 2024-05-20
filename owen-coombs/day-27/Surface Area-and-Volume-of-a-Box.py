078cf894ad77b0e0a74131ae71af85780924f23e
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-20 19:11:37 -0400

    Owen week 6 feedback

--- owen-coombs/day-27/Surface Area-and-Volume-of-a-Box.py
+++ owen-coombs/day-27/Surface Area-and-Volume-of-a-Box.py
@@ -1,4 +1,4 @@
-def get_size(w,h,d):
+def get_size(w, h, d):
     solution_array = []
     area = 2 * d * w + 2 * d * h + 2 * h * w
     volume = w * h * d
@@ -10,3 +10,17 @@ def get_size(w,h,d):
 
 # look up math solution lol
 # append to array
+
+"""
+    Ember's Feedback:
+    - rename this file to conform to the class standard
+    - Great job!
+"""
+
+# Alternative Solution
+
+
+def get_size(w, h, d):
+    area = 2 * (w * h + h * d + w * d)
+    volume = w * h * d
+    return [area, volume]
