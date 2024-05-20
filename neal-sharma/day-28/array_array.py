1faa2c66ea82ce4b201860251eedf84f6f6d3264
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-20 18:17:28 -0400

    Neal week 6 feedback

--- neal-sharma/day-28/array_array.py
+++ neal-sharma/day-28/array_array.py
@@ -2,8 +2,16 @@
 # return- the sum of the two arrays.
 # example- ([1, 2, 3], [4, 5, 6]), 21
 
-import math
-
 def array_plus_array(arr1,arr2):
     total = sum(arr1) + sum(arr2)
     return total
+
+"""
+    Ember's Feedback:
+    - Great job! Much more concise than JS :cries:
+"""
+
+
+# Alternative Solution
+def array_plus_array(arr1, arr2):
+    return sum(arr1 + arr2)
