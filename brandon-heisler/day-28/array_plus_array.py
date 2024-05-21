684a1278eff41ce5a8096d881feab4d2952dbaf4
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-21 10:15:57 -0400

    Brandon week 6 feedback

--- brandon-heisler/day-28/array_plus_array.py
+++ brandon-heisler/day-28/array_plus_array.py
@@ -1,8 +1,13 @@
 def array_plus_array(arr1,arr2):
     out_arr = arr1 + arr2
-    
+
     return sum(out_arr)
 
 # i want to return the sum of 2 arrays.
 # to do that ill combine the arrays and return the sum of the numbers
 # i found sum() which will do the trick in the return
+
+"""
+    Ember's Feedback:
+    - Great job! So much better syntax than JS :P
+"""
