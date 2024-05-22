cfe10f449c0162e3f91ae6dbe192229ad8318600
hpknollenberg <hpknollenberg@gmail.com>
2024-05-22 08:50:36 -0400

    day-38

--- henry-knollenberg/day-38/who_likes_it.py
+++ henry-knollenberg/day-38/who_likes_it.py
@@ -7,11 +7,11 @@
 def likes(names):
     if len(names) == 0:
         return 'no one likes this'
-    if len(names) == 1:
+    elif len(names) == 1:
         return f'{names[0]} likes this'
-    if len(names) == 2:
+    elif len(names) == 2:
         return f'{names[0]} and {names[1]} like this'
-    if len(names) == 3:
+    elif len(names) == 3:
         return f'{names[0]}, {names[1]} and {names[2]} like this'
     else:
         return f'{names[0]}, {names[1]} and {len(names) - 2} others like this'
