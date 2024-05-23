ce5c7b7e7ca24658e4de8968752ea4c1b9ef1014
nwils000 <nwils000@gmail.com>
2024-05-23 08:10:35 -0400

    finished day 39 katas

---
+++ nathan-wilson/day-39/create_phone_number.py
@@ -0,0 +1,14 @@
+def create_phone_number(n):
+    return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
+    
+# an array of integers
+
+# a string
+
+# ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890"
+
+# I could have a type error or not format the string correctly
+
+# Take the number and create a template literal for each number in order
+
+# I can do this
