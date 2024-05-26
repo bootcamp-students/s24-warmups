e565d8cb12b5007119bf71d8a6e760c2d95bff28
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-26 14:31:37 -0400

    Owen week 7 feedback, missing days 33 and 34

--- owen-coombs/day-31/century-from-year.py
+++ owen-coombs/day-31/century-from-year.py
@@ -1,7 +1,26 @@
 import math
 def century(year):
-    # Finish this :)
     return math.ceil(year / 100)
 
+
 # use math ceil
-#return mail.ceil and year / 100
+# return mail.ceil and year / 100
+
+"""
+    Ember's Feedback:
+    - Great work! :D
+    - make all .py and .sql file names snake_case
+"""
+
+
+# Alternative Solutions
+def century(year):
+    return year // 100 + (1 if year % 100 != 0 else 0)
+
+
+def century(year):
+    return (year + 99) // 100
+
+
+def century(year):
+    return (year - 1) // 100 + 1
--- owen-coombs/day-32/length-select-like.sql
+++ owen-coombs/day-32/length-select-like.sql
@@ -1,2 +1,9 @@
-SELECT first_name, last_name FROM Names
-  WHERE first_name LIKE '______%';
+SELECT first_name,
+  last_name
+FROM Names
+WHERE first_name LIKE '______%';
+
+/*
+  Ember's Feedback:
+  - Good job!
+*/
