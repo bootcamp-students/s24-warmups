84474fb9cac82873fb6c2459f55a21d3d53883bf
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-26 10:47:58 -0400

    Maddie week 7 feedback

--- maddie-carlson/day-34/abs-log.sql
+++ maddie-carlson/day-34/abs-log.sql
@@ -1,2 +1,9 @@
 -- return a table with two columns, abs and log where the values calculated accordingly
-SELECT ABS(number1) AS abs, LOG(64, number2) AS log FROM decimals
+SELECT ABS(number1) AS abs,
+    LOG(64, number2) AS log
+FROM decimals
+
+/*
+    Ember's Feedback:
+    - Good work!
+*/
