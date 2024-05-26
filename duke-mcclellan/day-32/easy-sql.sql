703eabe8b1ca922ad918abca7470ff845958330f
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-26 11:26:47 -0400

    Duke week 7 feedback

--- duke-mcclellan/day-32/easy-sql.sql
+++ duke-mcclellan/day-32/easy-sql.sql
@@ -1,3 +1,9 @@
-/*  SQL  */
-SELECT * FROM companies
+SELECT *
+FROM companies
 ORDER BY employees DESC
+
+/*
+    Ember's Feedback:
+    - Make all SQL files snake_case as well
+    - Great work! Order by is really common and useful
+*/
