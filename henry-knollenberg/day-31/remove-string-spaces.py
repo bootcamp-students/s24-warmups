3c4792cd455dddae1cc3db92d7dc3ac3a9479552
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-26 10:17:41 -0400

    Henry week 7 feedback

--- henry-knollenberg/day-31/remove-string-spaces.py
+++ henry-knollenberg/day-31/remove-string-spaces.py
@@ -1,5 +1,27 @@
 # split " "
-# join 
+# join
 
 def no_space(x):
     return "".join(x.split(" "))
+
+"""
+    Ember's Feedback:
+    - Great job!
+    - snake case this file name
+"""
+
+
+# Alternative Solution
+def no_space(x):
+    return "".join(x.split())
+
+
+def no_space(x):
+    return x.replace(" ", "")
+
+
+import re
+
+
+def no_space(x):
+    return re.sub("\s*", "", x)
--- henry-knollenberg/day-33/best-selling-books.sql
+++ henry-knollenberg/day-33/best-selling-books.sql
@@ -1,3 +1,10 @@
-SELECT * FROM books 
+SELECT *
+FROM books
 ORDER BY copies_sold DESC
 LIMIT 5
+
+/*
+    Ember's Feedback:
+    - Good job!
+    - Limit comes in handy when you want to do things like pagination
+*/
