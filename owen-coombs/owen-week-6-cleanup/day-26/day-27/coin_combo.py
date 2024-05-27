4095c10283496f69a43d0e3800dfdee78e55510b
Owen Coombs <owencoombs03@gmail.com>
2024-05-27 19:12:17 -0400

    added katas week 6
    added missing katas week 6

---
+++ owen-coombs/owen-week-6-cleanup/day-26/day-27/coin_combo.py
@@ -0,0 +1,22 @@
+##  an array needs to be returned
+##comment beside loops to understand later
+
+function coinCombo (cents) {
+  const coinValues = [25, 10, 5, 1];
+  
+  const coins = Array(coinValues.length).fill(0);
+  let currentCents = cents
+  for (const coin of coinValues) {
+    // if there are some coins left
+    while (currentCents >= coin) {
+      // how many cents are left
+      // how many times the current coins fit into the current cents
+      const remainder = currentCents % coin;
+      const increaseBy = (currentCents - remainder) / coin;
+      currentCents = currentCents % coin;
+      const index = coinValues.length - 1 - coinValues.indexOf(coin);
+      coins[index] += increaseBy;
+      }
+    }
+	return coins;
+}
