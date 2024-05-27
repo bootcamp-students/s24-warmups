0506ca62e0cb8bab66aed6ea3d1c87de1dd8ec7f
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-05-26 20:44:36 -0400

    Ryan week 8 feedback

--- ryan-anderson/day-39/create_phone_number.py
+++ ryan-anderson/day-39/create_phone_number.py
@@ -1,6 +1,16 @@
 def create_phone_number(n):
     return f'({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}'
-    
+
 
 # take input list of 10 digits and return them as (###) ###-####
-    # use f string to place digits in required format 
+# use f string to place digits in required format
+
+"""
+    Ember's Feedback:
+    - Great work!
+"""
+
+
+# Alternative Solution
+def create_phone_number(n):
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
