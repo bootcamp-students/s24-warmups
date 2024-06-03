5f102be8660ff7ddcc4713082a2ba4ba3605350f
oldmcdonnell <oldmcdonnell@gmail.com>
2024-06-03 09:09:09 -0400

    day 43

---
+++ eoin-mcdonnell/day-43/are_the_the_same.py
@@ -0,0 +1,18 @@
+##two arracys... compare if they are the same
+## with the sam multiplicities
+## the same mens
+## elements in b ar ein a sqeared??
+"""
+if it's multiply then it is the same 
+
+split the array or iterate
+"""
+
+def comp(array1, array2):
+    if type(array1) != list or type(array2) != list:
+        return False
+    result = []
+    for integer in array1:
+        result.append(integer * integer)
+    return sorted(result) == sorted(array2)
+    # your code
