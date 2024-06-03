f812f25d6a296263e35721225ad323098f4eb67d
Madeline Carlson <162224573+SpookyLamb@users.noreply.github.com>
2024-06-03 08:29:01 -0400

    Maddie week 9 submissions (#322)
    * Create buying-a-car.js
    
    * Create camel_case.py
    
    * Create maximum-subarray-sum.js
    
    * Create sliding_puzzle.py
    
    * Create same_arrays.py
    
    * Create scramblies.js

---
+++ maddie-carlson/day-42/sliding_puzzle.py
@@ -0,0 +1,20 @@
+def is_solved(board):
+    #function takes a list of lists showing a "board" of a sliding puzzle
+    #needs to return if the board is solved, aka all numbers are in order (1, 2, 3, 4), or not
+    #the board is always a square, so we can find the length of the array and the subarrays by checking the outer array
+    length = len(board)
+    
+    last_value = -1 #first value of a solved board should be 0
+    
+    #to solve, loop over the array to make sure each subsequent element is 1 greater than the last
+    for i in range(length): #board loop
+        for j in range(length): #row loop
+            cur_value = board[i][j]
+            if last_value + 1 == cur_value:
+                last_value = cur_value #set last value
+                continue #continuous (so far)
+            else:
+                return False #non-continuous, board isn't solved
+    
+    #if the loop completes without throwing a false, the board is solved
+    return True
