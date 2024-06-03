eb474f382b99e391b8816c0c65ab3fd90dc40ba8
Nathan Wilson <100631201+nwils000@users.noreply.github.com>
2024-06-03 08:19:26 -0400

    FIXING Nathan week 8 submissions (#304)
    * finished day 36 katas
    
    * finished day 27 katas
    
    * finished day 38
    
    * finished day 39 katas
    
    * finished day 41, 42, and 43
    
    * removed print statements
    
    * deleted week 9 files

---
+++ nathan-wilson/day-37/keep_hydrated.py
@@ -0,0 +1,14 @@
+def litres(time):
+    return time // 2
+
+# integer
+
+# integer
+
+# 2, 1
+
+# I could do the math wrong or overcomplicate it
+
+# take the time // 2 and return that
+
+# I can do this
