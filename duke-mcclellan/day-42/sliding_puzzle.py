a698005e763efed4c601b859fa3aae8b63caf682
Duke McClellan <162177816+dmcclellan3@users.noreply.github.com>
2024-06-10 13:53:09 -0400

    Duke week9 kata submissions (#328)
    * pushing kata submissions
    
    * correcting file names
    
    * day 42 submission
    
    * day-43 submission
    
    * day-44
    
    * week 9 katas completed

---
+++ duke-mcclellan/day-42/sliding_puzzle.py
@@ -0,0 +1,17 @@
+##Parameters - array
+##Return - boolean
+##Example - For example, a 2x2 solved grid:
+# [ [0, 1],
+#   [2, 3] ]
+# A 2x2 unsolved grid:
+
+# [ [2, 1],
+#   [0, 3] ]
+
+##concern - wrong method 
+##Explain - I want to be able to iterate over random boards and determine if they
+##pass or fail.  
+
+
+def is_solved(board):
+    return [cell for row in board for cell in row] == list(range(len(board)**2))
