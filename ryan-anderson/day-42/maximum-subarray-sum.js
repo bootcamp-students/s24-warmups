c315062543fba3d14940d944a0433533b548e3e5
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-06-10 03:12:11 -0400

    alternative solution update max subarray

--- ryan-anderson/day-42/maximum-subarray-sum.js
+++ ryan-anderson/day-42/maximum-subarray-sum.js
@@ -39,6 +39,7 @@ var maxSequence = function (arr) {
   - This solution is big O of n squared. You can get this to linear time complexity if you use kadane's algorithm.
   - This code would error if arr is undefined, just FYI. That is fine for a kata, but it is something to think about on the job.
 */
+
 // Refactor
 var maxSequence = function (arr) {
   let maxSum = 0;
@@ -52,5 +53,69 @@ var maxSequence = function (arr) {
       if (sum > maxSum) maxSum = sum;
     }
   }
+  return maxSum;
+};
+
+// Alternative Solution
+// https://www.youtube.com/watch?v=2MmGzdiKR9Y
+// This guy does a slightly better job of explaining Kadane's algorithm in spacial terms: https://www.youtube.com/watch?v=GrNSGC8Z2T0
+
+// O(n^3)
+var maxSequence = function (arr) {
+  let maxSum = 0;
+  // left bound
+  for (let i = 0; i < arr.length; i++) {
+    // right bound
+    for (let j = 0; j < arr.length; j++) {
+      let sumBetweenBounds = 0;
+      // get sum of elements between bounds
+      for (let h = i; h <= j; h++) {
+        sumBetweenBounds += arr[h];
+      }
+      // keep track of the largest sum of any subarray
+      if (sumBetweenBounds > maxSum) maxSum = sumBetweenBounds;
+    }
+  }
+
+  return maxSum;
+};
+
+// O(n^2)
+var maxSequence = function (arr) {
+  let maxSum = 0;
+  // left bound
+  for (let i = 0; i < arr.length; i++) {
+    let sumBetweenBounds = 0;
+    // right bound
+    for (let j = i; j < arr.length; j++) {
+      // sumBetweenBounds is the result of the last subarray sum, so by adding the current value -
+      // to it we are preventing the duplicate work of summing the entire subarray from left to right bounds
+      sumBetweenBounds += arr[j];
+      // keep track of the largest sum of any subarray
+      if (sumBetweenBounds > maxSum) maxSum = sumBetweenBounds;
+    }
+  }
+
+  return maxSum;
+};
+
+// O(n)
+var maxSequence = function (arr) {
+  // variable to store the maximum sum of all sub arrays
+  // this kata doesn't want negative results so that is why we use 0 instead of Number.NEGATIVE_INFINITY
+  let maxSum = 0;
+  // variable to store the max value of all subarrays that occurred to the left of the current index
+  let runningMax = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let currentValue = arr[i];
+    // the current value is it's own subarray
+    // the logic here is that by evaluating the max value as we move through the list, -
+    // we are calculating the maximum possible value of all subarray that occurred to the left of this index.
+    runningMax = Math.max(currentValue, runningMax + currentValue);
+    // because the above is calculating the maximum of a subArrays until that point in the list, -
+    // we want to store the maximum that could have occurred before this index.
+    maxSum = Math.max(maxSum, runningMax);
+  }
+
   return maxSum;
 };
