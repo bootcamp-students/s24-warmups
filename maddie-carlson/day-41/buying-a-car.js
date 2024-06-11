063d60f0eec19ad90db3c948a85e241ff9591de7
Ember Borja <borjaember@gmail.com>
2024-06-11 18:26:00 -0400

    Maddie week 9 feedback

--- maddie-carlson/day-41/buying-a-car.js
+++ maddie-carlson/day-41/buying-a-car.js
@@ -1,33 +1,43 @@
-function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
-  //function takes an old car price, a new car price, savings per month, and percent loss by month, all numbers
-  //needs to return a list of two numbers,
+function nbMonths(
+  startPriceOld,
+  startPriceNew,
+  savingPerMonth,
+  percentLossByMonth
+) {
+  // function takes an old car price, a new car price, savings per month, and percent loss by month, all numbers
+  // needs to return a list of two numbers,
   //  the first being a count of months and the second being the money left over
-  
-  //to solve, loop over each "month" until we have enough money to buy the new car
-  let totalSavings = 0
-  let months = 0
-  
+
+  // to solve, loop over each "month" until we have enough money to buy the new car
+  let totalSavings = 0;
+  let months = 0;
+
   while (totalSavings + startPriceOld < startPriceNew) {
-    months += 1 //increase month count
-    
-    if (months % 2 === 0) { //on even months, percent loss increases
-      percentLossByMonth += 0.5
+    months += 1; // increase month count
+
+    if (months % 2 === 0) {
+      // on even months, percent loss increases
+      percentLossByMonth += 0.5;
     }
-    
-    let oldLoss = startPriceOld * (percentLossByMonth / 100) //calculate losses
-    let newLoss = startPriceNew * (percentLossByMonth / 100)
-    
-    startPriceOld -= oldLoss
-    startPriceNew -= newLoss
-    
-    totalSavings += savingperMonth //increase savings
-    
-    console.log("month: ", months, " percent_loss: ", percentLossByMonth, " available: ", startPriceOld + totalSavings - startPriceNew)
+
+    let oldLoss = startPriceOld * (percentLossByMonth / 100); // calculate losses
+    let newLoss = startPriceNew * (percentLossByMonth / 100);
+
+    startPriceOld -= oldLoss;
+    startPriceNew -= newLoss;
+
+    totalSavings += savingPerMonth; // increase savings
   }
-  
-  //once we're out of the loop, we can calculate the remaining money after the car exchange
-  let remainder = totalSavings + startPriceOld - startPriceNew
-  remainder = Math.round(remainder) //round to nearest int
-  
-  return [months, remainder]
+
+  // once we're out of the loop, we can calculate the remaining money after the car exchange
+  let remainder = totalSavings + startPriceOld - startPriceNew;
+  remainder = Math.round(remainder); // round to nearest int
+
+  return [months, remainder];
 }
+
+/*
+  Ember's Feedback:
+  - Great work!
+  - This is practice for deciphering business logic in work tickets :P
+*/
--- maddie-carlson/day-43/same_arrays.py
+++ maddie-carlson/day-43/same_arrays.py
@@ -1,34 +1,55 @@
 def comp(array1, array2):
-    #function takes two arrays, and returns a boolean saying if the arrays "match"
-    #a matching array includes the same elements regardless of order and multiplicity (example given is squares)
-    
-    #first check to make sure neither of the arrays are null
+    # function takes two arrays, and returns a boolean saying if the arrays "match"
+    # a matching array includes the same elements regardless of order and multiplicity (example given is squares)
+
+    # first check to make sure neither of the arrays are null
     if array1 == None or array2 == None:
-        return False #return false, doesn't make sense
-    
-    #then check if the arrays are the same length
+        return False  # return false, doesn't make sense
+
+    # then check if the arrays are the same length
     if len(array1) != len(array2):
-        return False #can't possibly match if they aren't the same length
-    
-    #to solve, sort the arrays after taking care of any negative numbers
+        return False  # can't possibly match if they aren't the same length
+
+    # to solve, sort the arrays after taking care of any negative numbers
     for i in range(len(array1)):
         array1[i] = abs(array1[i])
         array2[i] = abs(array2[i])
-    
+
     array1.sort()
     array2.sort()
-    
-    #then loop over the arrays to make sure the arrays match
+
+    # then loop over the arrays to make sure the arrays match
     for i in range(len(array1)):
         num = array1[i]
         num2 = array2[i]
-        
-        if num * num == num2: #check squares
+
+        if num * num == num2:  # check squares
             continue
         elif num == num2 * num2:
             continue
-        else: #doesn't match
+        else:  # doesn't match
             return False
-    
-    #if the loop completes without incident, return True
+
+    # if the loop completes without incident, return True
     return True
+
+
+"""
+    Ember's Feedback:
+    - Good job getting to green!
+    - Don't directly modify parameter arrays! It is the same deal as in JS, you alter the list in other areas of code and will cause bugs.
+      I don't care that it is "just a kata" because this is a code behavior you can and should be practicing now if you want to get the most of this program!
+      By directly modify, I mean how you override the array elements like on line 15 and calling .sort() instead of using sorted()
+    - Squaring a number already makes it positive so the abs calls are unnecessary anyway.
+    - You can leverage the built in behavior of comparison operator for lists in python to make this code much more succinct.
+"""
+
+
+# Alternative Solution
+def comp(array1, array2):
+    try:
+        # list comprehension here squares all values of array1, -
+        # then we sort to leverage the built in comparison operator behavior for lists
+        return sorted([i**2 for i in array1]) == sorted(array2)
+    except:
+        return False
