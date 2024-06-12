3a701f56afdc07de5604fb739a931f9738644dec
Ember Borja <borjaember@gmail.com>
2024-06-12 14:17:58 -0400

    Owen week 9 feedback. Missing 2

--- owen-coombs/day-43/are_they_the_same.py
+++ owen-coombs/day-43/are_they_the_same.py
@@ -12,3 +12,16 @@ def comp(array1, array2):
 ## **2 is the math for squared 
 ## will use sort to sort the result in the array after they have been squared 
 ## compare sorrted array1 to sorted array 2 
+
+"""
+    Ember's Feedback:
+    - Great work!
+"""
+# Alternative Solution
+
+
+def comp(array1, array2):
+    try:
+        return sorted([i**2 for i in array1]) == sorted(array2)
+    except:
+        return False
--- owen-coombs/day-44/scramblies.js
+++ owen-coombs/day-44/scramblies.js
@@ -1,28 +1,37 @@
 function scramble(str1, str2) {
-    let checkStr = {}
-      
-      for (let char of str1){
-        if (checkStr[char]){
-          checkStr[char]++;
-        } else {
-          checkStr[char] = 1;
-        }
-      } 
-    // check for string 2 
-      for (let char of str2){
-        if (checkStr[char]){
-          checkStr[char]--;
-        }else {
-          return false
-        }
-      }
+  let checkStr = {}
     
-      return true;
+  for (let char of str1){
+    if (checkStr[char]){
+      checkStr[char]++;
+    } else {
+      checkStr[char] = 1;
     }
-    
-    
-    
-    // will loop through an object and increase count for each one char[char] if less then 1  for string 1
-    // return will either be a true or false 
-    // will do the same as string 1 but will decrease if greater then 0
-    // 
+  } 
+  
+  // check for string 2 
+  for (let char of str2){
+    if (checkStr[char]){
+      checkStr[char]--;
+    }else {
+      return false
+    }
+  }
+
+  return true;
+}
+  
+  
+  
+// will loop through an object and increase count for each one char[char] if less then 1  for string 1
+// return will either be a true or false 
+// will do the same as string 1 but will decrease if greater then 0
+
+/*
+  Ember's Feedback:
+  - Great job!!
+  - Make sure you understand why lines 5 and 14 work. If you attempt to access a property that -
+    does not exist, JS will return the value 'undefined' which is falsey.
+    Falsey means that the if statement is expecting a Boolean so if you don't give it one, it will coerce what you gave it into being a Boolean.
+    'Undefined' when cooerced becomes 'false'
+*/
