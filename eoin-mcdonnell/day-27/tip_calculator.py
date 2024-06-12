146cbd9d9909e34104fa07f09782b80a65adc992
Ember Borja <38664600+emberborja@users.noreply.github.com>
2024-06-12 13:55:31 -0400

    Eoin week 6 feedback (#238)
    sqashing commits to keep Main branch git history tidy.

--- eoin-mcdonnell/day-27/tip_calculator.py
+++ eoin-mcdonnell/day-27/tip_calculator.py
@@ -1,16 +1,54 @@
-#switch case in python
+# switch case in python
 # math ceil in python
 import math
+
+
 def calculate_tip(amount, rating):
-    #your code here
-    rates = {
-        'terrible':0,
-        'poor':5,
-        'good':10,
-        'great':15,
-        'excellent':20
-    }
+    rates = {"terrible": 0, "poor": 5, "good": 10, "great": 15, "excellent": 20}
     if not rating or rating.lower() not in rates:
-        return 'Rating not recognised'
+        return "Rating not recognised"
     else:
         return math.ceil(amount * rates[rating.lower()] / 100)
+
+
+"""
+    Ember's Feedback:
+    - Great job! I like that syntax in the if else
+"""
+
+
+# Alternative Solutions
+def calculate_tip(amount, rating):
+    how_was_service = {
+        "terrible": 0.0,
+        "poor": 0.05,
+        "good": 0.1,
+        "great": 0.15,
+        "excellent": 0.2,
+    }
+
+    rating = rating.lower()
+
+    return (
+        math.ceil(amount * how_was_service[rating])
+        if rating in how_was_service
+        else "Rating not recognised"
+    )
+
+
+def calculate_tip(amount, rating):
+    match rating.lower():
+        case "terrible":
+            mult_value = 0
+        case "poor":
+            mult_value = 0.05
+        case "good":
+            mult_value = 0.10
+        case "great":
+            mult_value = 0.15
+        case "excellent":
+            mult_value = 0.20
+        case _:
+            return "Rating not recognised"
+
+    return math.ceil(amount * mult_value)
--- eoin-mcdonnell/day-28/array_plus_array.py
+++ eoin-mcdonnell/day-28/array_plus_array.py
@@ -1,3 +1,14 @@
-#sum will add att the contents of the array in pythong
-def array_plus_array(arr1,arr2):
+# sum will add all the contents of the array in python
+def array_plus_array(arr1, arr2):
     return sum(arr1) + sum(arr2)
+
+
+"""
+    Ember's Feedback:
+    - Great job :D
+"""
+
+
+# Alternative Solution
+def array_plus_array(arr1, arr2):
+    return sum(arr1 + arr2)
